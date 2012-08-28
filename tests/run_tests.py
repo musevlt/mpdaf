d0d059e9fdf1c103b87ff7a6a02aca48daf6de4c
Laure Piqueras <laure.piqueras@univ-lyon1.fr>
2012-08-28 16:11:09 +0200

    import tests files

---
+++ tests/run_tests.py
@@ -0,0 +1,16 @@
+ 
+import unittest
+
+import os
+import sys
+sys.path.append('tests/drs')
+sys.path.append('tests/obj')
+
+from test_calibobj import TestCalibObj
+from test_rawobj import TestRawObj
+from test_coords import TestWCS
+from test_coords import TestWaveCoord
+from test_spectrum import TestSpectrum
+from test_image import TestImage
+from test_cube import TestCube
+
