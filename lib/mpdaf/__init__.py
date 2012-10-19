dddf48ffbf4949bfef4b4c6ee190339069b429ca
Laure Piqueras <laure.piqueras@univ-lyon1.fr>
2012-10-19 14:38:39 +0200

    minor version 1.0.1.01

--- lib/mpdaf/__init__.py
+++ lib/mpdaf/__init__.py
@@ -1,5 +1,5 @@
-__version__  = '1.0.1'
-__date__     = '2012/09/27 17:35'
+__version__  = '1.0.1.01'
+__date__     = '2012/10/19 14:33'
 
 import tools
 import obj
--- lib/mpdaf/obj/__init__.py
+++ lib/mpdaf/obj/__init__.py
@@ -39,8 +39,8 @@ USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 DAMAGE.
 
 """
-__version__  = '1.0.1'
-__date__     = '2012/09/27 17:35'
+__version__  = '1.0.1.01'
+__date__     = '2012/10/19 14:33'
 
 """
 Import the different submodules
