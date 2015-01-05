0b306d341b9b289199e7a8d158a5aa8e723958de
Simon Conseil <simon.conseil@univ-lyon1.fr>
2015-01-05 15:31:07 +0100

    Skip tests if files are missing.
    A better way to do this would be to create a decorator which would check if
    a file exists.

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 
 # Copyright (C) 2011  Centre de Recherche Astronomique de Lyon (CRAL)
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #     1. Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
-# 
+#
 #     2. Redistributions in binary form must reproduce the above
 #       copyright notice, this list of conditions and the following
 #       disclaimer in the documentation and/or other materials provided
 #       with the distribution.
-# 
+#
 #     3. The name of AURA and its representatives may not be used to
 #       endorse or promote products derived from this software without
 #       specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY CRAL ``AS IS'' AND ANY EXPRESS OR IMPLIED
 # WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 # MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -28,103 +28,113 @@
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 # USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 # DAMAGE.
-# 
+#
 
-from distutils.core import setup, Command, Extension
 import os
 import commands
-#import setuptools
+import subprocess
+# import setuptools
+
+from distutils.core import setup, Command, Extension
+
 
 class UnitTest(Command):
     user_options = []
+
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
+
     def run(self):
-        import sys,subprocess
-        #errno = subprocess.call(['python', 'tests/run_tests.py'])
-        errno = subprocess.call(['nosetests', '-v','-a speed=fast'])
+        errno = subprocess.call(['nosetests', '-v', '-a speed=fast'])
         raise SystemExit(errno)
+
 
 class MakeFusion(Command):
     user_options = []
+
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
+
     def run(self):
-        import subprocess,shutil,mpdaf.fusion
+        import shutil
+        import mpdaf.fusion
         errno = subprocess.call(['make', '-C', 'lib/mpdaf/fusion/'])
-        shutil.copy('lib/mpdaf/fusion/fusion_fit','/usr/local/bin/fusion_fit')
-        shutil.copy('lib/mpdaf/fusion/fusion_FSF','/usr/local/bin/fusion_FSF')
-        shutil.copy('lib/mpdaf/fusion/fusion_LSF','/usr/local/bin/fusion_LSF')
-        shutil.copy('lib/mpdaf/fusion/fusion_residual','/usr/local/bin/fusion_residual')
-        shutil.copy('lib/mpdaf/fusion/fusion_resampling','/usr/local/bin/fusion_resampling')
-        shutil.copy('lib/mpdaf/fusion/fusion_variance','/usr/local/bin/fusion_variance')
+        shutil.copy('lib/mpdaf/fusion/fusion_fit', '/usr/local/bin/fusion_fit')
+        shutil.copy('lib/mpdaf/fusion/fusion_FSF', '/usr/local/bin/fusion_FSF')
+        shutil.copy('lib/mpdaf/fusion/fusion_LSF', '/usr/local/bin/fusion_LSF')
+        shutil.copy('lib/mpdaf/fusion/fusion_residual', '/usr/local/bin/fusion_residual')
+        shutil.copy('lib/mpdaf/fusion/fusion_resampling', '/usr/local/bin/fusion_resampling')
+        shutil.copy('lib/mpdaf/fusion/fusion_variance', '/usr/local/bin/fusion_variance')
         errno = subprocess.call(['make', 'cleanall', '-C', 'lib/mpdaf/fusion/'])
         path = os.path.abspath(os.path.dirname(mpdaf.fusion.__file__))
-        shutil.copy('lib/mpdaf/fusion/examples/LSF_V1.fits',path + '/LSF_V1.fits')
+        shutil.copy('lib/mpdaf/fusion/examples/LSF_V1.fits', path + '/LSF_V1.fits')
 
-package_dir = {'mpdaf': 'lib/mpdaf/','mpdaf_user':'mpdaf_user/'}
+package_dir = {'mpdaf': 'lib/mpdaf/', 'mpdaf_user': 'mpdaf_user/'}
 if os.path.isfile('lib/mpdaf/fusion/__init__.py'):
-    packages = ['mpdaf','mpdaf.tools','mpdaf.obj','mpdaf.fusion','mpdaf.drs','mpdaf.MUSE','mpdaf_user']
+    packages = ['mpdaf', 'mpdaf.tools', 'mpdaf.obj', 'mpdaf.fusion', 'mpdaf.drs', 'mpdaf.MUSE', 'mpdaf_user']
 else:
-    packages = ['mpdaf','mpdaf.tools','mpdaf.obj','mpdaf.drs','mpdaf.MUSE','mpdaf_user']
+    packages = ['mpdaf', 'mpdaf.tools', 'mpdaf.obj', 'mpdaf.drs', 'mpdaf.MUSE', 'mpdaf_user']
 for path in os.listdir('mpdaf_user'):
-    if os.path.isdir('mpdaf_user/'+path+'/lib/'+path):        
-        package_dir['mpdaf_user.'+path] = 'mpdaf_user/'+path+'/lib/'+path
-        packages.append('mpdaf_user.'+path)
-    if os.path.isfile('mpdaf_user/'+path+'/__init__.py'):        
-        package_dir['mpdaf_user.'+path] = 'mpdaf_user/'+path+'/lib/'
-        packages.append('mpdaf_user.'+path)
+    if os.path.isdir('mpdaf_user/' + path + '/lib/' + path):
+        package_dir['mpdaf_user.' + path] = 'mpdaf_user/' + path + '/lib/' + path
+        packages.append('mpdaf_user.' + path)
+    if os.path.isfile('mpdaf_user/' + path + '/__init__.py'):
+        package_dir['mpdaf_user.' + path] = 'mpdaf_user/' + path + '/lib/'
+        packages.append('mpdaf_user.' + path)
         #package_dir[path] = 'mpdaf_user/'+path
-        #packages.append(path)
-    if os.path.isfile('mpdaf_user/'+path+'/'+path+'.py'):        
-        package_dir['mpdaf_user.'+path] = 'mpdaf_user/'+path
-        packages.append('mpdaf_user.'+path)
-        
+        # packages.append(path)
+    if os.path.isfile('mpdaf_user/' + path + '/' + path + '.py'):
+        package_dir['mpdaf_user.' + path] = 'mpdaf_user/' + path
+        packages.append('mpdaf_user.' + path)
+
 
 def options(*packages, **kw):
     flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
 
     err = commands.getstatusoutput("pkg-config --version")
-    if err[0] !=0 :
+    if err[0] != 0:
         raise ImportError(err[1])
 
     for package in packages:
-        err = commands.getstatusoutput("pkg-config %s"%package)
-        if err[0] !=0 :
+        err = commands.getstatusoutput("pkg-config %s" % package)
+        if err[0] != 0:
             raise ImportError(err[1])
-    
+
     for token in commands.getoutput("pkg-config --libs --cflags %s" % ' '.join(packages)).split():
-       if flag_map.has_key(token[:2]):
-           kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
-       else: # throw others to extra_link_args
-           kw.setdefault('extra_link_args', []).append(token)
+        if flag_map.has_key(token[:2]):
+            kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
+        else:  # throw others to extra_link_args
+            kw.setdefault('extra_link_args', []).append(token)
 
     kw.setdefault('libraries', []).append('m')
     kw.setdefault('extra_link_args', []).append('-fopenmp')
     kw.setdefault('extra_compile_args', []).append('-fopenmp')
 
-    for k, v in kw.iteritems(): # remove duplicated
+    for k, v in kw.iteritems():  # remove duplicated
         kw[k] = list(set(v))
     return kw
 
 
-setup(name = 'mpdaf',
-      version = '1.1',
-      description = 'MUSE Python Data Analysis Framework is a python framework in view of '
-                    'the analysis of MUSE data in the context of the GTO.',
-      url = 'http://urania1.univ-lyon1.fr/mpdaf/login',
-      requires = ['numpy (>= 1.0)', 'scipy (>= 0.10)', 'matplotlib','astropy','nose','PIL'],
-      package_dir = package_dir,
-      packages = packages,
+setup(name='mpdaf',
+      version='1.1',
+      description='MUSE Python Data Analysis Framework is a python framework '
+      'in view of the analysis of MUSE data in the context of the GTO.',
+      url='http://urania1.univ-lyon1.fr/mpdaf/login',
+      requires=['numpy (>= 1.0)', 'scipy (>= 0.10)', 'matplotlib', 'astropy',
+                'nose', 'PIL'],
+      package_dir=package_dir,
+      packages=packages,
       package_data={'mpdaf.drs': ['mumdatMask_1x1/*.fits']},
-      maintainer = 'Laure Piqueras',
-      maintainer_email = 'laure.piqueras@univ-lyon1.fr',
-      platforms = 'any', 
-      cmdclass = {'test': UnitTest, 'fusion': MakeFusion},
+      maintainer='Laure Piqueras',
+      maintainer_email='laure.piqueras@univ-lyon1.fr',
+      platforms='any',
+      cmdclass={'test': UnitTest, 'fusion': MakeFusion},
       ext_package='mpdaf',
       ext_modules=[Extension('libCmethods', ['src/subtract_slice_median.c', 'src/merging.c'], **options('cfitsio'))],
-     )
+      )
--- tests/drs/test_rawobj.py
+++ tests/drs/test_rawobj.py
@@ -1,22 +1,27 @@
 """Test on RawFile objects to be used with py.test."""
+
 import nose.tools
-from nose.plugins.attrib import attr
-
 import os
-import sys
 import numpy
+import unittest
 
+from nose.plugins.attrib import attr
 from mpdaf.drs import RawFile
 
-class TestRawObj():
-    
-    def setUp(self):
-        self.raw = RawFile("data/drs/raw.fits")
-        self.raw.progress = False
-        
-    def tearDown(self):
-        del self.raw
+DATA_PATH = "data/drs/raw.fits"
+DATA_MISSING = not os.path.exists(DATA_PATH)
 
+
+class TestRawObj(object):
+
+    def setUp(self):
+        try:
+            self.raw = RawFile(DATA_PATH)
+            self.raw.progress = False
+        except IOError:
+            pass
+
+    @unittest.skipIf(DATA_MISSING, "Missing test data (data/drs/raw.fits)")
     @attr(speed='slow')
     def test_init(self):
         """Raw objects: tests initialization"""
@@ -24,35 +29,37 @@ class TestRawObj():
         shape = numpy.shape(chan1.data)
         nose.tools.assert_equal(shape, (self.raw.ny, self.raw.nx))
 
+    @unittest.skipIf(DATA_MISSING, "Missing test data (data/drs/raw.fits)")
     @attr(speed='slow')
     def test_operator(self):
         """Raw objects: tests arithmetic functions"""
         chan1 = self.raw.get_channel("CHAN01")
-        value1 = chan1.data[32,28]
+        value1 = chan1.data[32, 28]
 
         out = self.raw - self.raw
         chan2 = out.get_channel("CHAN01")
-        value2 = chan2.data[32,28]
+        value2 = chan2.data[32, 28]
         nose.tools.assert_equal(value2, 0)
 
         out = self.raw + self.raw
         chan2 = out.get_channel("CHAN01")
-        value2 = chan2.data[32,28]
-        nose.tools.assert_equal(value2, 2*value1)
+        value2 = chan2.data[32, 28]
+        nose.tools.assert_equal(value2, 2 * value1)
 
         out = self.raw * self.raw
         chan2 = out.get_channel("CHAN01")
-        value2 = chan2.data[32,28]
+        value2 = chan2.data[32, 28]
         nose.tools.assert_equal(value2, value1 * value1)
 
         out = self.raw.sqrt()
         chan2 = out.get_channel("CHAN01")
-        value2 = chan2.data[32,28]
+        value2 = chan2.data[32, 28]
         nose.tools.assert_equal(value2, numpy.sqrt(value1))
-        
+
         del out
         del chan2
 
+    @unittest.skipIf(DATA_MISSING, "Missing test data (data/drs/raw.fits)")
     @attr(speed='slow')
     def test_copy(self):
         """Raw objects: tests copy"""
@@ -61,19 +68,20 @@ class TestRawObj():
         out2 = out.copy()
         del out
         chan2 = out2.get_channel("CHAN02")
-        value2 = chan2.data[24,12]
+        value2 = chan2.data[24, 12]
         nose.tools.assert_equal(value2, 0)
         del out2
 
+    @unittest.skipIf(DATA_MISSING, "Missing test data (data/drs/raw.fits)")
     @attr(speed='slow')
     def test_mask(self):
         """Raw objects: tests strimmed and overscan functionalities"""
-        overscan = self.raw[1].data[24,12]
-        pixel = self.raw[1].data[240,120]
+        overscan = self.raw[1].data[24, 12]
+        pixel = self.raw[1].data[240, 120]
         out = self.raw[1].trimmed() * 10
-        nose.tools.assert_equal(out.data[24,12],overscan)
-        nose.tools.assert_equal(out.data[240,120],10*pixel)
+        nose.tools.assert_equal(out.data[24, 12], overscan)
+        nose.tools.assert_equal(out.data[240, 120], 10 * pixel)
 
         out = self.raw[1].overscan() * 2
-        nose.tools.assert_equal(out.data[24,12],2*overscan)
-        nose.tools.assert_equal(out.data[240,120],pixel)
+        nose.tools.assert_equal(out.data[24, 12], 2 * overscan)
+        nose.tools.assert_equal(out.data[240, 120], pixel)
