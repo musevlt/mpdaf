3892b10fc4c42b22ee791d77f3bd440b729a51cb
Simon Conseil <simon.conseil@univ-lyon1.fr>
2015-01-16 15:38:12 +0100

    mask_column is now 2x faster.

--- lib/mpdaf/drs/pixtable.py
+++ lib/mpdaf/drs/pixtable.py
@@ -1913,41 +1913,41 @@ class PixTable(object):
         out : :class:`mpdaf.drs.PixTableMask`
         """
         d = {'class': 'PixTable', 'method': 'mask_column'}
-        mask = np.zeros(self.nrows).astype('bool')
-        if maskfile is not None:
-            xpos = self.get_xpos()
-            ypos = self.get_ypos()
-            xpos_sky, ypos_sky = self.get_pos_sky(xpos=xpos, ypos=ypos)
-            del xpos, ypos
-            ima_mask = Image(maskfile)
-            from scipy import ndimage
-            label = ndimage.measurements.label(ima_mask.data.data)[0]
-            for i in np.unique(label):
-                if i != 0:
-                    try:
-                        ksel = np.where(label == i)
-                        item = (slice(min(ksel[0]), max(ksel[0]) + 1, None), \
-                                slice(min(ksel[1]), max(ksel[1]) + 1, None))
-                        coord = ima_mask.wcs[item].get_range()
-                        step = ima_mask.wcs[item].get_step()
-                        x0 = min(coord[0][1] - step[1]/2, coord[1][1] - step[1]/2)
-                        x1 = max(coord[0][1] + step[1]/2, coord[1][1] + step[1]/2)
-                        y0 = min(coord[0][0] - step[0]/2, coord[1][0] - step[0]/2)
-                        y1 = max(coord[0][0] + step[0]/2, coord[1][0] + step[0]/2)
-                        ksel = np.where((xpos_sky>x0) & (xpos_sky <x1) & \
-                                        (ypos_sky>y0) & (ypos_sky<y1))
-                        if verbose:
-                            msg = 'masking object %i %g<x<%g %g<y<%g (%i pixels)' \
-                            % (i,x0, x1, y0, y1, len(ksel[0]))
-                            self.logger.info(msg, extra=d)
-                        pix = np.array(ima_mask.wcs.sky2pix(\
-                              zip(ypos_sky[ksel], xpos_sky[ksel]), \
-                              nearest=True), dtype=int)
-                        imask = [ima_mask[p[0],p[1]]!=0 for p in pix]
-                        mask[ksel] = np.logical_or(mask[ksel], imask)
-                    except:
-                        pass
-            del ima_mask
+
+        mask = np.zeros(self.nrows, dtype=np.bool)
+        if maskfile is None:
+            return mask
+
+        pos = np.array(self.get_pos_sky()[::-1]).T
+        xpos_sky = pos[:, 1]
+        ypos_sky = pos[:, 0]
+
+        ima_mask = Image(maskfile)
+        label = ndimage.measurements.label(ima_mask.data.data)[0]
+        ulabel = np.unique(label)
+        ulabel = ulabel[ulabel > 0]
+
+        for i in ulabel:
+            try:
+                ksel = np.where(label == i)
+                item = (slice(min(ksel[0]), max(ksel[0]) + 1, None),
+                        slice(min(ksel[1]), max(ksel[1]) + 1, None))
+                wcs = ima_mask.wcs[item]
+                coord = wcs.get_range()
+                step = wcs.get_step()
+                y0, x0 = coord.min(axis=0) - step / 2
+                y1, x1 = coord.max(axis=0) + step / 2
+                ksel = np.where((xpos_sky > x0) & (xpos_sky < x1) &
+                                (ypos_sky > y0) & (ypos_sky < y1))
+                if verbose:
+                    msg = 'masking object %i %g<x<%g %g<y<%g (%i pixels)' % (
+                        i, x0, x1, y0, y1, len(ksel[0]))
+                    self.logger.info(msg, extra=d)
+                pix = ima_mask.wcs.sky2pix(pos[ksel], nearest=True)
+                mask[ksel] |= (ima_mask.data.data[pix[:,0], pix[:,1]] != 0)
+            except Exception as e:
+                self.logger.warning('masking object %i failed', i, extra=d)
+
         return PixTableMask(maskfile, mask)
 
     def old_subtract_slice_median(self, mask=None, norm='sky'):
