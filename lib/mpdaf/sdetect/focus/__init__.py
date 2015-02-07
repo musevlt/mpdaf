f1c0c673f32f365de3b959a6308c31218b75b92a
Simon Conseil <simon.conseil@univ-lyon1.fr>
2015-02-07 15:42:07 +0100

    Don't print the msg about Focus each time Mpdaf is imported.

--- lib/mpdaf/sdetect/focus/__init__.py
+++ lib/mpdaf/sdetect/focus/__init__.py
@@ -1,20 +1,16 @@
-"""Fast Object Cube Search module
+""" FOCUS: Fast Object CUbe Search
 
-Algorithm from Carole Clastre (carole.clastres@univ-lyon1.fr)
+This software has been developped by Carole Clastres under the supervision of
+David Mary (Lagrange institute, University of Nice) and ported to python by
+Laure Piqueras (CRAL).
+
+The project is funded by the ERC MUSICOS (Roland Bacon, CRAL). Please contact
+Carole for more info at carole.clastres@univ-lyon1.fr
 
 """
+
 __version__ = '1.1.15'
 __date__ = '2015/02/06'
 
-"""
-Import the different submodules
-"""
-
-from source_detect import SourceCatalog
-from source_detect import SourceDetect3D
-import logging
-
-print 'FOCUS: Fast Object CUbe Search'
-print 'This software has been developped by Carole Clastres under the supervision of David Mary (Lagrange institute, University of Nice) and ported to python by Laure Piqueras (CRAL).'
-print 'The project is funded by the ERC MUSICOS (Roland Bacon, CRAL). Please contact Carole for more info at carole.clastres@univ-lyon1.fr'
- 
+from .source_detect import SourceCatalog
+from .source_detect import SourceDetect3D
