dddf26942456ea94b9d1d1f45bcefb35841439b0
Simon Conseil <simon.conseil@univ-lyon1.fr>
2015-02-24 16:12:34 +0100

    Add a script to create a white-light image from a cube.

---
+++ lib/mpdaf/scripts/make_white_image.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import argparse
+from mpdaf.obj import Cube
+from time import time
+
+
+def make_white_image(inputfile, outputfile, verbose=False):
+    t0 = time()
+    print('Creating white light image {}'.format(outputfile))
+    cube = Cube(inputfile)
+    if verbose:
+        cube.info()
+
+    if cube.fscale == 1:
+        cube.fscale = 1.e-20
+    im = cube.mean(axis=0)
+    im.write(outputfile)
+    if verbose:
+        print('Execution time {:.3f} seconds.'.format(time() - t0))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Make a white-light image from a cube, by computing the '
+                    'mean for each pixel along the spectral axis.')
+    parser.add_argument('input_cube', help='Input cube (FITS file).')
+    parser.add_argument('output_image', help='Output image (FITS file).')
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help='verbose flag')
+    args = parser.parse_args()
+    make_white_image(args.input_cube, args.output_image, verbose=args.verbose)
+
+
+if __name__ == '__main__':
+    main()
