605c3dca381eed2fd0879e9a05e4ebcb1bbb5290
piqueras <laure.piqueras@univ-lyon1.fr>
2015-05-05 14:12:30 +0200

    update FOCUS detection code to be compatible with new Source object

--- lib/mpdaf/sdetect/__init__.py
+++ lib/mpdaf/sdetect/__init__.py
@@ -1,7 +1,6 @@
 __version__ = '1.1.16'
 __date__ = '2015/03/16'
 
-from catalog import SourceCatalog
 from source import Source
 from source import SourceList
 from catalog import Catalog
--- lib/mpdaf/sdetect/focus.py
+++ lib/mpdaf/sdetect/focus.py
@@ -13,9 +13,12 @@ import logging
 from scipy.stats import t
 from scipy import ndimage
 import os.path
+from astropy.table import Table
 
 from ..obj import Cube, Image
-from ..sdetect import SourceCatalog
+from ..sdetect import Source, SourceList
+
+__version__ = 1.0
 
 
 class FOCUS(object):
@@ -71,7 +74,7 @@ class FOCUS(object):
         exposures = Cube(self.expmap).data.data
  
         # Weighted cube
-        self.logger.info('Compute weighted cube', extra=d)
+        self.logger.info('FOCUS - Compute weighted cube', extra=d)
         if self.cube.var is not None:
             cube_w = cube / np.sqrt(self.cube.var)
         else:
@@ -79,9 +82,9 @@ class FOCUS(object):
         cube_w = np.nan_to_num(cube_w)
  
         # Compute the p-values with student cumulative distribution function with exposures-1 degrees of freedom
-        msg = 'Computing the p-values using student cumulative distribution function with expmap-1 degrees of freedom'
+        msg = 'FOCUS - Computing the p-values using student cumulative distribution function with expmap-1 degrees of freedom'
         self.logger.info(msg, extra=d)
-        self.logger.info('Please note that it takes some time ...', extra=d)
+        self.logger.info('FOCUS - Please note that it takes some time ...', extra=d)
  
         ksel = np.where(exposures < 2)  # seg fault if not ...
         exposures[ksel] = 2
@@ -110,8 +113,9 @@ class FOCUS(object):
         -------
         imaobj :  :class:`mpdaf.obj.Image`
                   Image containing maximum lambda value of the detected objects
-        catalog : :class:`mpdaf.obj.SourceCatalog`
-                  Catalog listing the center of mass of the detected components
+        sources : list< :class:`mpdaf.sdetect.Source` >
+                  List of source objects.
+                  Each source contains only the center of mass of the detected component
                   and the maximum wavelengths
         """
         d = {'class': 'SourceDetect3D', 'method': 'quick_detection'}
@@ -119,7 +123,7 @@ class FOCUS(object):
         cube = self.cube.data.data
  
         # Weighted cube
-        self.logger.info('Computing weighted cube', extra=d)
+        self.logger.info('FOCUS - Computing weighted cube', extra=d)
         if self.cube.var is not None:
             cube_w = cube / np.sqrt(self.cube.var)
         else:
@@ -133,14 +137,14 @@ class FOCUS(object):
         Ny = self.cube.shape[1]
  
         # Filter to remove deviant pixels
-        self.logger.info('Filtering to remove deviant pixels', extra=d)
+        self.logger.info('FOCUS - Filtering to remove deviant pixels', extra=d)
         cartemin = np.amin(cube_w, axis=0)
         cartemax = np.amax(cube_w, axis=0)
  
         # p_values
         pval_t = p_values.data.data
  
-        self.logger.info('Detecting where p-values<%g' % p0, extra=d)
+        self.logger.info('FOCUS - Detecting where p-values<%g' % p0, extra=d)
         # Find the minimum of the p-values
         pval_inf_t = np.amin(pval_t, axis=0)
         lambda_cube = np.argmax(cube_w, axis=0)
@@ -152,7 +156,7 @@ class FOCUS(object):
         detect = np.zeros((Ny, Nx))
         detect[filtre] = wavelength[filtre]
  
-        self.logger.info('Finding the 8 connected components', extra=d)
+        self.logger.info('FOCUS - Finding the 8 connected components', extra=d)
         # Count the objects detected
         # Find the 8 connected components in binary image
         structure = ndimage.morphology.generate_binary_structure(2, 8)
@@ -163,16 +167,33 @@ class FOCUS(object):
  
         # Count the contiguous pixels (at least 2)
         n = 0
-        Obj = np.zeros((Nx, Ny))
-        catalog = dict()
+        Obj = np.zeros((Ny, Nx))
+        #catalog = dict()
+        sources = SourceList()
+        origin=('FOCUS', __version__, self.cube.filename)
+        expmap = os.path.basename(self.expmap)
+        if p_values.filename is None:
+            pval_file = ''
+        else:
+            pval_file = os.path.basename(p_values.filename)
         for i in range(1, Nobjects):
             ksel = np.where(label[0] == i)
             if (len(ksel[0]) > 1):
                 n += 1
                 Obj[ksel] = detect[ksel]
                 coord = self.cube.wcs.pix2sky(pos[i - 1])[0]
-                catalog[tuple(coord)] = np.unique(detect[ksel])
-        self.logger.info('%d objects detected' % n, extra=d)
+                #create source object
+                ra = coord[1]
+                dec = coord[0]
+                rows = [[l] for l in np.unique(detect[ksel])]
+                lines = Table(names=['LBDA_OBS'], rows=rows)
+                extras = {}
+                extras['pvalues'] = (pval_file, 'p_values')
+                extras['p0'] = (p0, 'p0')
+                extras['expmap'] = (expmap, 'Exposures map')
+                sources.append(Source.from_data(ID=n, ra=ra, dec=dec, origin=origin, 
+                                            lines=lines, extras=extras))
+        self.logger.info('FOCUS - %d objects detected' % n, extra=d)
  
         # resulted image
         imaobj = Image(wcs=self.cube.wcs, data=Obj)
@@ -180,25 +201,4 @@ class FOCUS(object):
         imaobj.mask_selection(ksel)
         #imaobj.plot(vmin=4900, vmax=9300, colorbar='v')
  
-        # catalog
-        try:
-            wcs = self.cube.data_header['CUNIT1']
-            wave = self.cube.data_header['CUNIT3']
-        except:
-            wcs = ''
-            wave = ''
- 
-        if p_values.filename is None:
-            pval_file = ''
-        else:
-            pval_file = os.path.basename(p_values.filename)
- 
-        cat = SourceCatalog(catalog, wcs, wave,
-                            "obj.SourceDetect3D.quick_detection",
-                            ['cube', 'expmap', 'pvalues', 'p0'],
-                            [os.path.basename(self.cube.filename),
-                             os.path.basename(self.expmap),
-                             pval_file, p0],
-                            ['cube', 'expmpa', 'pvalues', 'p0'])
- 
-        return imaobj, cat
+        return imaobj, sources
