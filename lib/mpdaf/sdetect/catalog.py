8500a0ae9ca1ad2234e4925a061ef782f4c48d07
piqueras <laure.piqueras@univ-lyon1.fr>
2015-05-06 12:17:36 +0200

    Catalog: take into account types of columns

--- lib/mpdaf/sdetect/catalog.py
+++ lib/mpdaf/sdetect/catalog.py
@@ -1,16 +1,9 @@
 
-from astropy.io import fits as pyfits
-from astropy.io.fits import Column
 from astropy.coordinates import SkyCoord, search_around_sky
 from astropy import units as u
 
-import datetime
 import logging
-import warnings
 import numpy as np
-from collections import OrderedDict
-
-from ..tools.fits import add_mpdaf_method_keywords
 
 from astropy.table import Table, hstack
 
@@ -37,35 +30,56 @@ class Catalog(Table):
         sources : list< :class:`mpdaf.sdetect.Source` >
         """ 
         # union of all headers keywords without mandatory FITS keywords
-        names_hdr = list(set(np.concatenate([source.header.keys() 
-                                         for source in sources]))
-                     - set(['SIMPLE', 'BITPIX', 'NAXIS',
-                            'EXTEND','DATE','AUTHOR']))
+        
+        h = sources[0].header
+        d = dict(zip(h.keys(), [type(v) for v in h.values()]))
+        for source in sources[1:]:
+            h = source.header
+            d.update(dict(zip(h.keys(), [type(v) for v in h.values()])))
+        d = {key: value for key, value in d.items() if key not in ['SIMPLE', 'BITPIX', 'NAXIS','EXTEND','DATE','AUTHOR']}
+        names_hdr = d.keys()
+        dtype_hdr = d.values()
         #sort mandatory keywords
-        names_hdr.insert(0, names_hdr.pop(names_hdr.index('ID')))
-        names_hdr.insert(1, names_hdr.pop(names_hdr.index('RA')))
-        names_hdr.insert(2, names_hdr.pop(names_hdr.index('DEC')))
-        names_hdr.insert(3, names_hdr.pop(names_hdr.index('ORIGIN')))
-        names_hdr.insert(4, names_hdr.pop(names_hdr.index('ORIGIN_V')))
-        names_hdr.insert(5, names_hdr.pop(names_hdr.index('CUBE')))
+        index = names_hdr.index('ID')
+        names_hdr.insert(0, names_hdr.pop(index))
+        dtype_hdr.insert(0, dtype_hdr.pop(index))
+        index = names_hdr.index('RA')
+        names_hdr.insert(1, names_hdr.pop(index))
+        dtype_hdr.insert(1, dtype_hdr.pop(index))
+        index = names_hdr.index('DEC')
+        names_hdr.insert(2, names_hdr.pop(index))
+        dtype_hdr.insert(2, dtype_hdr.pop(index))
+        index = names_hdr.index('ORIGIN')
+        names_hdr.insert(3, names_hdr.pop(index))
+        dtype_hdr.insert(3, dtype_hdr.pop(index))
+        index = names_hdr.index('ORIGIN_V')
+        names_hdr.insert(4, names_hdr.pop(index))
+        dtype_hdr.insert(4, dtype_hdr.pop(index))
+        index = names_hdr.index('CUBE')
+        names_hdr.insert(5, names_hdr.pop(index))
+        dtype_hdr.insert(5, dtype_hdr.pop(index))
          
         #lines
         llines = [len(source.lines) for source in sources if source.lines is not None]
         if len(llines) != 0:
             lmax = max(llines)
+            d = {}
+            for source in sources:
+                if source.lines is not None:
+                    for col in source.lines.colnames:
+                        d[col] = source.lines.dtype[col]
             if lmax ==1:
-                names_lines = list(OrderedDict.fromkeys(np.concatenate(
-                            [source.lines.colnames for source in sources
-                             if source.lines is not None])))
+                names_lines = sorted(d)
+                dtype_lines = [d[key] for key in sorted(d)]
             else:
                 names_lines = []
-                inames_lines = list(OrderedDict.fromkeys(np.concatenate(
-                            [source.lines.colnames for source in sources
-                             if source.lines is not None])))
+                inames_lines = sorted(d)
                 for i in range(1,lmax+1):
                     names_lines += [col+'%03d'%i for col in inames_lines]
+                dtype_lines = [d[key] for key in sorted(d)]*lmax
         else:
             names_lines = []
+            dtype_lines = []
             
         #magnitudes
         lmag = [len(source.mag) for source in sources if source.mag is not None]
@@ -82,33 +96,34 @@ class Catalog(Table):
         if len(lz) != 0:
             names_z = list(set(np.concatenate([source.z['Z_DESC'] for source in sources
                              if source.z is not None])))
-            names_z = ['z_%s'%z for z in names_z]
+            names_z = ['Z_%s'%z for z in names_z]
             names_z += ['%s_ERR'%z for z in names_z]
             names_z.sort()
         else:
             names_z = []
+            
         
         data_rows = []
         for source in sources:
             # header
             h = source.header
             keys = h.keys()
-            row = [h[key] if key in keys else '' for key in names_hdr]
+            row = [h[key] if key in keys else None for key in names_hdr]
             # lines
             if len(llines) != 0:
                 if source.lines is None:
-                    row += ['' for key in names_lines]
+                    row += [None for key in names_lines]
                 else:
                     keys = source.lines.colnames
                     if lmax ==1:
-                        row += [source.lines[key][0] if key in keys else '' for key in names_lines]
+                        row += [source.lines[key][0] if key in keys else None for key in names_lines]
                     else:
                         n = len(source.lines)
-                        row += [source.lines[key[:-3]][int(key[-3:])-1] if key[:-3] in keys and int(key[-3:])<=n else '' for key in names_lines]
+                        row += [source.lines[key[:-3]][int(key[-3:])-1] if key[:-3] in keys and int(key[-3:])<=n else None for key in names_lines]
             #magnitudes
             if len(lmag) != 0:
                 if source.mag is None:
-                    row += ['' for key in names_mag]
+                    row += [None for key in names_mag]
                 else:
                     keys = source.mag['BAND']
                     for key in names_mag:
@@ -117,11 +132,11 @@ class Catalog(Table):
                         elif key[-4:]=='_ERR' and key[:-4] in keys:
                             row += [float(source.mag['MAG_ERR'][source.mag['BAND']==key[:-4]])]
                         else:
-                            row += [""]
+                            row += [None]
             #redshifts
             if len(lz) !=0:
                 if source.z is None:
-                    row += ['' for key in names_z]
+                    row += [None for key in names_z]
                 else:
                     keys = source.z['Z_DESC']
                     for key in names_z:
@@ -131,14 +146,39 @@ class Catalog(Table):
                         elif key[-4:]=='_ERR' and key[:-4] in keys:
                             row += [float(source.z['Z_ERR'][source.z['Z_DESC']==key[:-4]])]
                         else:
-                            row += [""]
+                            row += [None]
                     
             # final row
             data_rows.append(row)
-        
+            
+        dtype = dtype_hdr
+        #lines
+        if len(llines) != 0:
+#             dtype += ['<f8' for i in range(len(names_lines))]
+            dtype += dtype_lines
+        #magnitudes
+        if len(lmag) != 0:
+            dtype += ['<f8' for i in range(len(names_mag))]
+        #redshifts
+        if len(lz) !=0:
+            dtype += ['<f8' for i in range(len(names_z))]
+            
         #create Table
         names = names_hdr + names_lines + names_mag + names_z
-        return cls(rows=data_rows, names=names)    
+        t = cls(rows=data_rows, names=names, masked=True, dtype=dtype)
+        
+        #format
+        t['ID'].format = '%d'
+        t['RA'].format = '%.7f'
+        t['DEC'].format = '%.7f'
+        for names in names_z:
+            t[names].format = '%.6f'
+        for names in names_lines:
+            if names[:4]=='LBDA':
+                t[names].format = '%0.2f'
+            if names[:4]=='FLUX':
+                t[names].format = '%0.4f'
+        return t   
     
     def match(self, cat2, radius=1):
         """ Matchs elements of the current catalog with an other (in RA, DEC).
