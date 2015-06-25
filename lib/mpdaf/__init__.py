1addaf6f58ad0c3774120b0b3d665a8b4ebc5f52
piqueras <laure.piqueras@univ-lyon1.fr>
2015-06-25 17:03:47 +0200

    split logging method in 2 methods (steam_handler/file_handler)

--- lib/mpdaf/__init__.py
+++ lib/mpdaf/__init__.py
@@ -17,23 +17,44 @@ CPU = 0
 # logging
 
 
-def setup_logging(level, logfile):
+# def setup_logging(level, logfile):
+#     logger = logging.getLogger('mpdaf corelib')
+#     logger.setLevel(level)
+# 
+#     formatter = logging.Formatter('%(asctime)s [%(levelname)s] {mpdaf corelib %(class)s.%(method)s} %(message)s')
+#     file_handler = RotatingFileHandler(logfile, 'a', 1000000, 1)
+#     file_handler.setLevel(level)
+#     file_handler.setFormatter(formatter)
+#     logger.addHandler(file_handler)
+# 
+#     steam_handler = logging.StreamHandler()
+#     steam_handler.setLevel(level)
+#     formatter = logging.Formatter('[%(levelname)s] %(message)s')
+#     steam_handler.setFormatter(formatter)
+#     logger.addHandler(steam_handler)
+# 
+# try:
+#     setup_logging(logging.DEBUG, 'mpdaf.log')
+# except:
+#     pass
+
+def setup_logging(level):
     logger = logging.getLogger('mpdaf corelib')
-    logger.setLevel(logging.DEBUG)
-
-    formatter = logging.Formatter('%(asctime)s [%(levelname)s] {mpdaf corelib %(class)s.%(method)s} %(message)s')
-    file_handler = RotatingFileHandler(logfile, 'a', 1000000, 1)
-    file_handler.setLevel(level)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-
+    logger.handlers = []
+    logger.setLevel(level)
+  
     steam_handler = logging.StreamHandler()
     steam_handler.setLevel(level)
     formatter = logging.Formatter('[%(levelname)s] %(message)s')
     steam_handler.setFormatter(formatter)
     logger.addHandler(steam_handler)
-
-try:
-    setup_logging(logging.DEBUG, 'mpdaf.log')
-except:
-    pass
+    
+def setup_logfile(level, logfile):
+    logger = logging.getLogger('mpdaf corelib')
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s] {mpdaf corelib %(class)s.%(method)s} %(message)s')
+    file_handler = RotatingFileHandler(logfile, 'a', 1000000, 1)
+    file_handler.setLevel(level)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    
+setup_logging(logging.DEBUG)
