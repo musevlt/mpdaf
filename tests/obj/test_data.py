d02e53aaf93c9c081f700604c698dfe334292075
Simon Conseil <simon.conseil@univ-lyon1.fr>
2015-10-08 15:14:12 +0200

    Uncomment DataArray tests

--- tests/obj/test_data.py
+++ tests/obj/test_data.py
@@ -13,22 +13,22 @@ TESTIMG = join(DATADIR, 'data', 'obj', 'a370II.fits')
 TESTSPE = join(DATADIR, 'data', 'obj', 'Spectrum_lines.fits')
 
 
-class TestDataArray(object):
+@attr(speed='fast')
+def test_fits_img():
+    hdu = fits.open(TESTIMG)
+    data = DataArray(filename=TESTIMG)
+    nose.tools.assert_equal(data.shape, hdu[0].data.shape)
 
-    # @attr(speed='fast')
-    # def test_fits_img(self):
-    #     hdu = fits.open(TESTIMG)
-    #     data = DataArray(filename=TESTIMG)
-    #     nose.tools.assert_equal(data.shape, hdu[0].data.shape)
 
-    # @attr(speed='fast')
-    # def test_fits_spectrum(self):
-    #     hdu = fits.open(TESTSPE)
-    #     data = DataArray(filename=TESTSPE)
-    #     nose.tools.assert_equal(data.shape, hdu[1].data.shape)
+@attr(speed='fast')
+def test_fits_spectrum():
+    hdu = fits.open(TESTSPE)
+    data = DataArray(filename=TESTSPE)
+    nose.tools.assert_equal(data.shape, hdu[1].data.shape)
 
-    @attr(speed='fast')
-    def test_from_np(self):
-        data = np.arange(10)
-        d = DataArray(data=data)
-        nose.tools.assert_tuple_equal(d.shape, data.shape)
+
+@attr(speed='fast')
+def test_from_np():
+    data = np.arange(10)
+    d = DataArray(data=data)
+    nose.tools.assert_tuple_equal(d.shape, data.shape)
