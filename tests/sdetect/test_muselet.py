80023f00a0aeff63dd777b47acbbb6e2f28dacde
Simon Conseil <simon.conseil@univ-lyon1.fr>
2015-11-09 20:25:55 +0100

    Fix SEA and muselet tests with hardcoded paths.

--- tests/sdetect/test_muselet.py
+++ tests/sdetect/test_muselet.py
@@ -1,16 +1,19 @@
 """Test on muselet script."""
 import nose.tools
+import os
 from nose.plugins.attrib import attr
 
 from mpdaf.sdetect import muselet
 
+DATADIR = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                       '..', '..', 'data', 'sdetect')
 
-class TestMuselet():
 
-    @attr(speed='slow')
-    def test_muselet(self):
-        """test MUSELET"""
-        continuum, single, raw = muselet('data/sdetect/minicube.fits', nbcube=False, del_sex=True)
-        nose.tools.assert_equal(len(continuum), 1)
-        nose.tools.assert_equal(len(single), 8)
-        nose.tools.assert_equal(len(raw), 39)
+@attr(speed='slow')
+def test_muselet():
+    """test MUSELET"""
+    continuum, single, raw = muselet(os.path.join(DATADIR, 'minicube.fits'),
+                                     nbcube=False, del_sex=True)
+    nose.tools.assert_equal(len(continuum), 1)
+    nose.tools.assert_equal(len(single), 8)
+    nose.tools.assert_equal(len(raw), 39)
--- tests/sdetect/test_sea.py
+++ tests/sdetect/test_sea.py
@@ -1,5 +1,7 @@
 """Test on SEA algorythm."""
+
 import nose.tools
+import os
 from nose.plugins.attrib import attr
 
 from mpdaf.obj import Cube, Image
@@ -7,15 +9,16 @@ from mpdaf.sdetect import SEA
 
 from astropy.table import Table
 
+DATADIR = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                       '..', '..', 'data', 'sdetect')
 
-class TestSEA():
 
-    @attr(speed='slow')
-    def test_SEA(self):
-        """test SEA"""
-        cube = Cube('data/sdetect/minicube.fits')
-        ima = Image('data/sdetect/a478hst.fits')
-        cat = Table.read('../mpdaf.git/data/sdetect/cat.txt', format='ascii')
-        sources = SEA(cat, cube, {'hst': ima})
+@attr(speed='slow')
+def test_SEA():
+    """test SEA"""
+    cube = Cube(os.path.join(DATADIR, 'minicube.fits'))
+    ima = Image(os.path.join(DATADIR, 'a478hst.fits'))
+    cat = Table.read(os.path.join(DATADIR, 'cat.txt'), format='ascii')
+    sources = SEA(cat, cube, {'hst': ima})
 
-        nose.tools.assert_equal(len(sources), 8)
+    nose.tools.assert_equal(len(sources), 8)
