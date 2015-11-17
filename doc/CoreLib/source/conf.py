a7decf2ce349c60e08357f722ec48084f5b7bab6
piqueras <laure.piqueras@univ-lyon1.fr>
2015-11-17 11:46:44 +0100

    import sphinx_rtd_theme in the sphinx configuration file

--- doc/CoreLib/source/conf.py
+++ doc/CoreLib/source/conf.py
@@ -9,6 +9,10 @@
 
 # -- General configuration ----------------------------------------------------
 
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 # If your documentation needs a minimal Sphinx version, state it here.
 # needs_sphinx = '1.0'
 
