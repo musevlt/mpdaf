66ff8e3ac8e3f21749a9ab2263b87ca8a93305ab
Simon Conseil <simon.conseil@univ-lyon1.fr>
2015-12-16 12:46:23 +0100

    Add the possibility to print logging messages with colors.

--- lib/mpdaf/log.py
+++ lib/mpdaf/log.py
@@ -1,9 +1,32 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import os
+import sys
+
+# The background is set with 40 plus the number of the color, and the
+# foreground with 30
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = [30 + i
+                                                         for i in range(8)]
+COLORS = {
+    'DEBUG': BLUE,
+    'INFO': GREEN,
+    'WARNING': YELLOW,
+    'ERROR': RED,
+    'CRITICAL': MAGENTA,
+}
+# These are the sequences need to get colored ouput
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[1;{0}m{1}{2}"
+BOLD_SEQ = "\033[1m"
 
 
-def setup_logging(level=logging.DEBUG):
+def colored(text, color):
+    return COLOR_SEQ.format(color, text, RESET_SEQ)
+
+
+def setup_logging(level=logging.DEBUG, color=False,
+                  fmt='[%(levelname)s] %(message)s'):
     """Setup logging to stdout."""
     logger = logging.getLogger('mpdaf')
     logger.handlers = []
@@ -11,7 +34,11 @@ def setup_logging(level=logging.DEBUG):
 
     steam_handler = logging.StreamHandler()
     steam_handler.setLevel(level)
-    formatter = logging.Formatter('[%(levelname)s] %(message)s')
+    if (color and os.isatty(sys.stdout.fileno()) and
+            not sys.platform.startswith('win')):
+        formatter = ColoredFormatter(fmt)
+    else:
+        formatter = logging.Formatter(fmt)
     steam_handler.setFormatter(formatter)
     logger.addHandler(steam_handler)
 
@@ -26,3 +53,15 @@ def setup_logfile(level=logging.DEBUG, logfile='mpdaf.log'):
     file_handler.setLevel(level)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
+
+
+class ColoredFormatter(logging.Formatter):
+    def format(self, record):
+        level = record.levelname
+        process = record.process
+        record.levelname = colored(level, COLORS[level])
+        record.process = colored(process, 30 + process % 8)
+        s = logging.Formatter.format(self, record)
+        record.levelname = level
+        record.process = process
+        return s
