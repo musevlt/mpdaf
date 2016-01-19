c0fa25a2d389837894e8eb55351e71700f52a273
Martin Shepherd <mcs.shepherd@gmail.com>
2016-01-19 16:35:21 +0100

    Import various python 2/3 compatibility modules.

--- tests/obj/test_data.py
+++ tests/obj/test_data.py
@@ -1,4 +1,12 @@
 """Test on DataArray objects."""
+
+# Import the recommended python 2 -> 3 compatibility modules.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import nose.tools
 from nose.plugins.attrib import attr
 
