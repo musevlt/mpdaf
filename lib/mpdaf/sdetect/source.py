Fixed a problem regarding the calculation of the angle to rotate an image.
This issue was introduced by the conversion of rotation angles from a
pixel oriented convention to the convention which is usually used in
astronomy. In astronomy rotations are in the right-hand coordinate
system defined by Right Ascension and declination on the sky. In this
convention positive rotations rotate the sky in the direction from
north towards east (ie. a right-handed rotation from the positive Dec
axis towards the positive RA axis).  Because the RA axis is usually
drawn to the left, despite it being the positive axis, a positive
rotation on the sky generally causes an anticlockwise rotation on
people's displays. For example, in ds9, when one rotates an image
using the cursor, the angle increases as the image is rotated
anticlockwise, and decreases when rotated clockwise.

Following this change, if an image is currently at an angle of
current_angle, and one wants to rotate it to the new angle,
target_angle, then the rotation to be applied is:

  rotation_angle = target_angle - current_angle

Previously, target_angle and current_angle were swapped in this
equation, which is what caused the discrepancy in
source.py. Note that rearranging the above gives:

  target_angle = current_angle + rotation_angle

which is simple to understand and easy to remember.

lib/mpdaf/sdetect/source.py
@@ -1008,9 +1008,9 @@ class Source(object):
                                         unit_center=u.deg, unit_size=unit_size)
                 uniq = np.unique(subima.data.data)
                 if ((uniq == 0) | (uniq == 1)).all():
-                    subima = subima.rotate(pa - pa_white, order=0)
+                    subima = subima.rotate(pa_white - pa, order=0)
                 else:
-                    subima = subima.rotate(pa - pa_white)
+                    subima = subima.rotate(pa_white - pa)
                 subima = subima.subimage((self.dec, self.ra), size, minsize=minsize,
                                          unit_center=u.deg, unit_size=unit_size)
             else: