update Image.rebin_mean to be correct with the current implementation of the variance

lib/mpdaf/obj/image.py
@@ -3099,6 +3099,9 @@ class Image(DataArray):
         # successive groups of 'factor[0] x factor[1]' pixels. Note
         # that the following uses np.ma.mean(), which takes account of
         # masked pixels.
+        
+        if self.var is not None:
+            var = self.masked_var.copy()
 
         self.data = self.data.reshape(
             newshape[0], factor[0], newshape[1], factor[1]).mean(1).mean(2)
@@ -3110,7 +3113,7 @@ class Image(DataArray):
         # in that particular sum.
 
         if self.var is not None:
-            self.var = self.var.reshape(newshape[0], factor[0], newshape[1], factor[1]).sum(1).sum(2) / unmasked**2
+            self.var = (var.reshape(newshape[0], factor[0], newshape[1], factor[1]).sum(1).sum(2) / unmasked**2).data
 
         # Any pixels in the output array that come from zero unmasked
         # pixels of the input array should be masked.

tests/obj/test_image.py
@@ -415,9 +415,11 @@ def test_rebin_mean():
     # Given the number of pixels averaged in each of the above means,
     # we thus expect the variance array to look as follows.
 
-    expected = np.ma.array(data=[[0.5,   0.25], [0.25, 0.125], [0.0,  0.0]],
-                           mask=[[False,False], [False,False], [True,True]])
-    nose.tools.assert_true(np.ma.allclose(image2.var, expected))
+#     expected = np.ma.array(data=[[0.5,   0.25], [0.25, 0.125], [0.0,  0.0]],
+#                            mask=[[False,False], [False,False], [True,True]])
+#     nose.tools.assert_true(np.ma.allclose(image2.var, expected))
+    expected = np.array([[0.5,   0.25], [0.25, 0.125], [0.0,  0.0]])
+    nose.tools.assert_true(np.allclose(image2.var, expected))
 
     # Check the WCS information.
 