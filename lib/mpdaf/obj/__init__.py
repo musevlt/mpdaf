9d243cf34d1bd44abceef4cb8a4577f8ab677268
Simon Conseil <simon.conseil@univ-lyon1.fr>
2016-04-29 14:57:10 +0200

    Simplify mpdaf.obj imports and import objs functions.

--- lib/mpdaf/obj/__init__.py
+++ lib/mpdaf/obj/__init__.py
@@ -3,13 +3,6 @@ from .coords import *
 from .cube import *
 from .cubelist import *
 from .data import *
-from .image import composite_image
-from .image import Gauss2D
-from .image import gauss_image
-from .image import Image
-from .image import make_image
-from .image import mask_image
-from .image import Moffat2D
-from .image import moffat_image
-from .spectrum import Gauss1D
-from .spectrum import Spectrum
+from .image import *
+from .objs import *
+from .spectrum import *
