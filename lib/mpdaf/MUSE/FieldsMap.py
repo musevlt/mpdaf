4ee5a7d3a9b11944c6291a59f69cb004a1e5aca0
piqueras <laure.piqueras@univ-lyon1.fr>
2016-06-14 17:10:40 +0200

    add PSF and FieldMap classes in order to an image by a set of FSF (mosaic)

---
+++ lib/mpdaf/MUSE/FieldsMap.py
@@ -0,0 +1,181 @@
+"""Copyright 2010-2016 CNRS/CRAL
+
+This file is part of MPDAF.
+
+MPDAF is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version
+
+MPDAF is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MPDAF.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from __future__ import absolute_import, division
+
+from astropy.io import fits
+import numpy as np
+from scipy.interpolate import griddata
+from scipy.signal import fftconvolve
+import six
+from six.moves import range
+from six.moves import zip
+
+class FieldsMap(object):
+    """Class to work with the mosaic field map.
+    """
+
+    def __init__(self, filename, nfields=None, **kwargs):
+        """Class to work with the mosaic field map.
+        
+        Parameters
+        ----------
+        filename : FITS file name
+                   Name of the file containing the field map.
+                   Use ext='FIELDMAP' to read the field map from an
+                   extension the MUSE data cube.
+        nfields : integer
+                  Number of fields.
+        """
+        if nfields is None:
+            self.nfields = fits.getval(filename, 'NFIELDS')
+        else:
+            self.nfields = nfields
+        self.data = fits.getdata(filename, **kwargs)
+
+    def get_field_mask(self, field_name):
+        """Return an array with non-zeros values for pixels matching a field.
+
+        ``field_name`` can be an integer (between 1 and nfields+1) or a string (e.g.
+        UDF-03).
+
+        """
+        if isinstance(field_name, six.string_types):
+            field_name = int(field_name)
+        return (self.data & 2**field_name).astype(bool)
+
+    def get_pixel_fields(self, y, x):
+        """Return a list of fields that cover a given pixel (y, x)."""
+        ind = reversed("{0:010b}".format(self.data[y, x])[:-1])
+        fields = ('UDF-%02d' % i for i in range(1, self.nfields+1))
+        return [field for field, i in zip(fields, ind) if i == '1']
+        
+    def get_pixel_fields_indexes(self, y, x):
+        """Return a list of fields indexes (between 0 and nfields) 
+        that cover a given pixel (y, x)."""
+        ind = reversed("{0:010b}".format(self.data[y, x])[:-1])
+        indexes = (i for i in range(self.nfields))
+        return [index for index, i in zip(indexes, ind) if i == '1']
+    
+    def compute_weights(self):
+        """Return a list of weight maps (one per fields).
+        The weight gives the influence of the field for each pixel.
+        In the overlap area the weight changes linearly to have a
+        smooth transition.
+        """
+        p, q = np.meshgrid(range(self.data.shape[0]),
+                           range(self.data.shape[1]), 
+                           sparse=False, indexing='ij')
+    
+        # compute the mask for each field
+        fmaps = []
+        for i in range(1, self.nfields+1):
+            fmaps.append(self.get_field_mask(i))
+       
+        several = (np.sum(fmaps, axis=0) > 1)
+
+        w = []
+        s = None
+        for m in fmaps:
+            # pixels just in one field
+            ksel = np.where(np.logical_or(~m, ~several))
+            # pixels just in this field
+            pd = list(ksel[0])
+            qd = list(ksel[1])
+            z = m[ksel].astype(np.float)
+            wmap = griddata((pd, qd), z, (p, q), method='linear')
+            w.append(wmap)
+            if s is None:
+                s = wmap.copy()
+            else:
+                s += wmap
+            
+        ksel = np.where((s!=0) & (s!=1))
+        for wmap in w:
+            wmap[ksel] /= s[ksel]
+
+        return w
+    
+    def get_FSF(self, y, x, kernels, weights=None):
+        """Return the local FSF.
+        
+        Parameters
+        ----------
+        y       : integer
+                  Pixel coordinate along the y-axis.
+        x       : integer
+                  Pixel coordinate along the x-axis.
+        kernels : list of np.array
+                  List of FSF.
+        weights : list of np.array
+                  List of corresponding weights maps.
+                  Computed by compute_weights by default.
+        """
+        if weights is None:
+            weights = self.compute_weights()
+        fields = self.get_pixel_fields_indexes(y, x)
+        if len(fields) == 0:
+            return None
+        elif len(fields) == 1:
+            return kernels[fields[0]]
+        else:
+            i0 = fields[0]
+            FSF = weights[i0] * kernels[i0]
+            for i in fields[1:]:
+                FSF *= weights[i] * kernels[i]
+            return FSF
+    
+
+    def variable_PSF_convolution(self, img, kernels, weights=None):
+        """ Function used for the convolution of an image by a set of PSF.
+    
+        We use shift-variant blur techniques to model the variation of the PSF.
+    
+        Reference: Denis, L. Thiébaut E., Soulez F., Becker J.-M. and Mourya R. 
+               “Fast approximations of shift-variant blur“,
+               International Journal of Computer Vision,
+               Springer Verlag, 115(3), 253-278 (2015)
+
+        Parameters
+        ----------
+        img     : np.array
+                  Image to convolve.
+        kernels : list(np.array)
+                  List of convolution kernels.
+        weights : list of np.array
+                  List of corresponding weights maps.
+                  Computed by compute_weights by default.
+        """
+        if weights is None:
+            weights = self.compute_weights()
+        # kernels and weights shall have the same length
+        if len(kernels) != len(weights):
+            raise IOError('kernels and weights shall have the same length')
+        #img and weights shall have the same shape
+        if img.shape != weights[0].shape:
+            raise IOError('img and weights shall have the same shape')
+
+        convolved_img = np.zeros_like(img)
+
+        # build a weighting map per PSF and convolve
+        for i in range(self.nfields):
+            convolved_img = convolved_img \
+                        + fftconvolve(weights[i]*img, kernels[i], mode='same')
+
+        return convolved_img
+    
+
--- lib/mpdaf/MUSE/PSF.py
+++ lib/mpdaf/MUSE/PSF.py
@@ -18,63 +18,62 @@ along with MPDAF.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import, division
 
+from astropy.convolution import Model2DKernel
+from astropy.modeling.functional_models import Moffat2D
+import astropy.units as u
 import numpy as np
 from scipy import special
+
+from six.moves import range
 
 
 class LSF(object):
 
-    """This class manages MUSE LSF models.
-
-    :param type: type of LSF
-        if type is 'qsim_v1', This is a simple model where the LSF
-        is supposed to be constant over the filed of view.
-        It uses a simple parametric model of variation with wavelength.
-
-        The model is a convolution of a step function with a gaussian.
-        The resulting function is then sample by the pixel size.
-        The slit width is assumed to be constant (2.09 pixels).
-        The gaussian sigma parameter is a polynomial approximation
-        of order 3 with wavelength.
-    :type type: 'qsim_v1'
+    """This class offers Line Spread Function models for MUSE.
 
     Attributes
     ----------
 
-    type (string) : LSF type.
+    typ (string) : LSF type.
     """
 
-    def __init__(self, type="qsim_v1"):
+    def __init__(self, typ="qsim_v1"):
         """Manages LSF model.
 
-        :param type: type of LSF
-            if type is 'qsim_v1', This is a simple model
-            where the LSF is supposed
-            to be constant over the filed of view.
-            It uses a simple parametric model of variation with wavelength.
-
-            The model is a convolution of a step function with a gaussian.
-            The resulting function is then sample by the pixel size.
-            The slit width is assumed to be constant (2.09 pixels).
-            The gaussian sigma parameter is a polynomial approximation
-            of order 3 with wavelength.
-        :type type: 'qsim_v1'
+        Parameters
+        ----------
+        typ : string
+              type of LSF
+              
+        qsim_v1 : simple model where the LSF is supposed to be constant over
+        the FoV. It is a convolution of a step function with a Gaussian.
+        The resulting function is then sample by the pixel size.
+        The slit width is assumed to be constant (2.09 pixels).
+        The Gaussian sigma parameter is a polynomial approximation of order 3
+        with wavelength.
         """
-        self.type = type
+        self.typ = typ
 
     def get_LSF(self, lbda, step, size, **kargs):
-        """returns an array containing the LSF.
+        """Return an array containing the LSF.
 
-        :param lbda: wavelength value in A
-        :type lbda: float
-        :param step: size of the pixel in A
-        :type step: float
-        :param size: number of pixels
-        :type size: odd integer
-        :param kargs: kargs can be used to set LSF parameters.
-        :rtype: np.array
+        Parameters
+        ----------
+        lbda : float
+               Wavelength value in A.
+        step : float
+               Size of the pixel in A.
+        size : odd integer
+               Number of pixels.
+        kargs : dict
+                kargs can be used to set LSF parameters.
+               
+        Returns
+        -------
+        out : array
+              array containing the LSF
         """
-        if self.type == "qsim_v1":
+        if self.typ == "qsim_v1":
             T = lambda x: np.exp((-x ** 2) / 2.0) + np.sqrt(2.0 * np.pi) \
                 * x * special.erf(x / np.sqrt(2.0)) / 2.0
             c = np.array([-0.09876662, 0.44410609, -0.03166038, 0.46285363])
@@ -92,21 +91,29 @@ class LSF(object):
             y2 = (y + h_2) / sig
 
             lsf = T(y2 + dy_2) - T(y2 - dy_2) - T(y1 + dy_2) + T(y1 - dy_2)
+        else:
+            raise IOError('Invalid LSF type')
 
         lsf /= lsf.sum()
         return lsf
 
     def size(self, lbda, step, epsilon, **kargs):
-        """returns the LSF size in pixels.
+        """Return the LSF size in pixels.
 
-        :param lbda: wavelength value in A
-        :type lbda: float
-        :param step: size of the pixel in A
-        :type step: float
-        :param epsilon: this factor is used to determine the size of LSF
-            (min(LSF)<max(LSF)*epsilon)
-        :type epsilon: float
-        :rtype: integer
+        Parameters
+        ----------
+        lbda : float
+               Wavelength value in A.
+        step : float
+               Size of the pixel in A.
+        epsilon : float
+                  This factor is used to determine the size of LSF
+                  min(LSF) < max(LSF)*epsilon
+        
+        Returns
+        -------
+        out : integer
+              Size in pixels.
         """
         x0 = lbda
         # x = np.array([x0])
@@ -118,3 +125,176 @@ class LSF(object):
             diff = epsilon * g[1] - g[0]
         size = k * 2 + 1
         return size
+    
+def Moffat(step_arcsec, Nfsf, beta, fwhm):
+    """Compute FSF with a Moffat function
+
+    Parameters
+    ----------
+    step_arcsec : float
+                  Size of the pixel in arcsec.
+    Nfsf        : int
+                  Spatial dimension of the FSF.
+    beta        : float
+                  Power index of the Moffat.
+    fwhm        : float or array of float
+                  Moffat fwhm in arcsec.
+    
+
+    Returns
+    -------
+    PSF_Moffat : array (Nz, Nfsf, Nfsf)
+                 MUSE PSF
+    fwhm_pix   : array (Nz)
+                 fwhm of the PSF in pixels
+    fwhm_arcsec : array (Nz)
+                  fwhm of the PSF in arcsec
+    """
+    # conversion fwhm arcsec -> pixels
+    fwhm_pix = fwhm / step_arcsec
+
+    # alpha coefficient in pixel
+    alpha = fwhm_pix / (2 * np.sqrt(2**(1 / beta) - 1))
+    
+    if np.isscalar(alpha):
+        moffat = Moffat2D(1, 0, 0, alpha, beta)
+        moffat_kernel = Model2DKernel(moffat, x_size=Nfsf, y_size=Nfsf)
+        PSF_Moffat = moffat_kernel.array
+        # Normalization
+        PSF_Moffat = PSF_Moffat / np.sum(PSF_Moffat)
+    else:
+        Nz = alpha.shape[0]
+        PSF_Moffat = np.empty((Nz, Nfsf, Nfsf))
+        for i in range(Nz):
+            moffat = Moffat2D(1, 0, 0, alpha[i], beta)
+            moffat_kernel = Model2DKernel(moffat, x_size=Nfsf, y_size=Nfsf)
+            PSF_Moffat [i,:,:]= moffat_kernel.array
+        # Normalization
+        PSF_Moffat = PSF_Moffat / np.sum(PSF_Moffat, axis=(1, 2))\
+            [:, np.newaxis, np.newaxis]
+                    
+    return PSF_Moffat, fwhm_pix
+    
+def MOFFAT1(lbda, step_arcsec, Nfsf, beta, a, b):
+    """Compute PSF with a Moffat function
+
+    Parameters
+    ----------
+    lbda        : float or array of float
+                  Wavelength values in Angstrom.
+    step_arcsec : float
+                  Size of the pixel in arcsec
+    Nfsf        : int
+                  Spatial dimension of the FSF.
+    beta        : float
+                  Power index of the Moffat.
+    a           : float
+                  Moffat parameter in arcsec (fwhm=a+b*lbda)
+    b           : float
+                  Moffat parameter (fwhm=a+b*lbda)
+
+    Returns
+    -------
+    PSF_Moffat : array (Nz, Nfsf, Nfsf)
+                 MUSE PSF
+    fwhm_pix   : array (Nz)
+                 fwhm of the PSF in pixels
+    fwhm_arcsec : array (Nz)
+                  fwhm of the PSF in arcsec
+    """
+    fwhm_arcsec = a + b*lbda
+    PSF_Moffat, fwhm_pix = Moffat(step_arcsec, Nfsf, beta, fwhm_arcsec)
+    return PSF_Moffat, fwhm_pix, fwhm_arcsec
+    
+class FSF(object):
+
+    """This class offers Field Spread Function models for MUSE.
+
+    Attributes
+    ----------
+
+    typ (string) : FSF type.
+    """
+
+    def __init__(self, typ="MOFFAT1"):
+        """Manages LSF model.
+
+        Parameters
+        ----------
+        typ : string
+              type of LSF
+              
+        
+        MOFFAT1: Moffat function with a FWHM which varies linearly with the
+        wavelength. Parameters:
+         - beta (float) Power index of the Moffat.
+         - a (float) constant in arcsec which defined the FWHM (fwhm=a+b*lbda)
+         - b (float) constant which defined the FWHM (fwhm=a+b*lbda)
+        
+        """
+        self.typ = typ
+
+    def get_FSF(self, lbda, step, size, **kargs):
+        """Return an array containing the FSF for a given wavelength.
+
+        Parameters
+        ----------
+        lbda  : float
+                Wavelength value in A.
+        step  : float
+                Size of the pixel in arcsec.
+        size  : integer
+                Number of pixels.
+        kargs : dict
+                kargs can be used to set LSF parameters.
+               
+        Returns
+        -------
+        FSF         : array (size, size)
+                      MUSE FSF
+        fwhm_pix    : float
+                      fwhm of the FSF in pixels
+        fwhm_arcsec : array
+                      fwhm of the FSF in arcsec
+        """
+        if self.typ == "MOFFAT1":
+            return MOFFAT1(lbda, step, size, **kargs)
+        else:
+            raise IOError('Invalid FSF type')
+        
+    def get_FSF_cube(self, cube, size, **kargs):
+        """Return a cube of FSFs corresponding to the MUSE data cube
+        given in input:
+        - a FSF per MUSE spectral pixels,
+        - the step of the FSF pixel is equal to the spatial step of the
+          MUSE data cube.
+
+        Parameters
+        ----------
+        cube  : `mpdaf.obj.Cube`
+                The wavelength coordinates of the MUSE spectral pixels.
+                Wavelength value in A.
+        size  : integer
+                FSF size in pixels.
+        kargs : dict
+                kargs can be used to set LSF parameters.
+               
+        Returns
+        -------
+        FSF         : array (cube.shape[0], size, size)
+                      Cube containing MUSE FSF (one per wavelength)
+        fwhm_pix    : array(cube.shape[0])
+                      fwhm of the FSF in pixels
+        fwhm_arcsec : array(cube.shape[0])
+                      fwhm of the FSF in arcsec
+        """
+        # size of the pixel in arcsec.
+        step = cube.wcs.get_step(unit=u.arcsec)[0]
+        # wavelength coordinates of the MUSE spectral pixels.
+        lbda = cube.wave.coord()
+        if self.typ == "MOFFAT1":
+            return MOFFAT1(lbda, step, size, **kargs)
+        else:
+            raise IOError('Invalid FSF type')
+        
+
