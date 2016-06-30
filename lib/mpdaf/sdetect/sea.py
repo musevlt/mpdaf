d437a28ab9daf1768f02f3aae5fd961ecc81490e
Simon Conseil <simon.conseil@univ-lyon1.fr>
2016-06-30 12:40:13 +0200

    Update license for files in lib/mpdaf/ to BSD

--- lib/mpdaf/sdetect/sea.py
+++ lib/mpdaf/sdetect/sea.py
@@ -1,34 +1,35 @@
-"""Copyright 2010-2016 CNRS/CRAL
+"""
+Copyright (c) 2010-2016 CNRS / Centre de Recherche Astrophysique de Lyon
+Copyright (c) 2015-2016 Laure Piqueras <laure.piqueras@univ-lyon1.fr>
+Copyright (c) 2015-2016 Simon Conseil <simon.conseil@univ-lyon1.fr>
+Copyright (c)      2016 Roland Bacon <roland.bacon@univ-lyon1.fr>
 
-This file is part of MPDAF.
+All rights reserved.
 
-MPDAF is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
 
-MPDAF is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
 
-You should have received a copy of the GNU General Public License
-along with MPDAF.  If not, see <http://www.gnu.org/licenses/>.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
 
+3. Neither the name of the copyright holder nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
 
-sea.py contains SpectExtractAnd[nothing], the first part of
-SpecExtractAndWeb software developed by Jarle.
-
-This software has been developed by Jarle Brinchmann (University of Leiden)
-and ported to python by Laure Piqueras (CRAL).
-
-It takes a MUSE data cube and a catalogue of objects and extracts small
-sub-cubes around each object. From this it creates narrow-band images and
-eventually spectra. To do the latter it is necessary to run an external routine
-which runs sextractor on the images to define spectrum extraction apertures.
-
-Please contact Jarle for more info at jarle@strw.leidenuniv.nl
-
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 from __future__ import absolute_import, division
 
