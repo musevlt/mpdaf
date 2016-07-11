a5d14409c9d6aec6c4ad58d0496baeb21df17459
Simon Conseil <simon.conseil@univ-lyon1.fr>
2016-07-11 16:10:11 +0200

    Add a fast muselet test, using only a slice of the cube

--- tests/sdetect/test_muselet.py
+++ tests/sdetect/test_muselet.py
@@ -2,25 +2,31 @@
 
 from __future__ import absolute_import, division, print_function
 
-import shutil
-import tempfile
 import pytest
-
 from mpdaf.sdetect.muselet import muselet
 
-from ..utils import get_data_file
+
+def test_muselet_fast(tmpdir, minicube):
+    """test MUSELET"""
+    outdir = str(tmpdir)
+    filename = str(tmpdir.join('cube.fits'))
+    cube = minicube[1800:2000, :, :]
+    cube.write(filename, savemask='nan')
+    print('Working directory:', outdir)
+    cont, single, raw = muselet(filename, nbcube=False, del_sex=True,
+                                workdir=outdir)
+    assert len(cont) == 1
+    assert len(single) == 7
+    assert len(raw) == 22
 
 
 @pytest.mark.slow
-def test_muselet():
+def test_muselet_full(tmpdir, minicube):
     """test MUSELET"""
-    try:
-        outdir = tempfile.mkdtemp(prefix='muselet.')
-        print('Working directory:', outdir)
-        cont, single, raw = muselet(get_data_file('sdetect', 'minicube.fits'),
-                                    nbcube=False, del_sex=True, workdir=outdir)
-        assert len(cont) == 1
-        assert len(single) == 8
-        assert len(raw) == 39
-    finally:
-        shutil.rmtree(outdir)
+    outdir = str(tmpdir)
+    print('Working directory:', outdir)
+    cont, single, raw = muselet(minicube.filename, nbcube=False, del_sex=True,
+                                workdir=outdir)
+    assert len(cont) == 1
+    assert len(single) == 8
+    assert len(raw) == 39
