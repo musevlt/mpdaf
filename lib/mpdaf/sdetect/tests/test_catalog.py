086e1f8b1f231c98126e6dda933048f32f842e2d
Simon Conseil <simon.conseil@univ-lyon1.fr>
2016-07-28 12:02:56 +0200

    42 is the answer :)

--- lib/mpdaf/sdetect/tests/test_catalog.py
+++ lib/mpdaf/sdetect/tests/test_catalog.py
@@ -9,6 +9,7 @@ from mpdaf.sdetect import Catalog
 
 from numpy.testing import assert_array_equal
 
+
 @pytest.mark.xfail(sys.version_info >= (3, 3),
                    reason="not compatible with python 3")
 def test_catalog(source1, source2):
@@ -19,19 +20,14 @@ def test_catalog(source1, source2):
     cat = Catalog.from_sources([source1, source2], fmt='working')
     print(cat)
     assert len(cat) == 2
-    assert len(cat.colnames) == 29
+    assert len(cat.colnames) == 42
     assert list(cat['ID']) == [1, 32]
     assert list(cat['CUBE_V']) == ['0.1', '0.2']
     assert_array_equal(source1.lines['LINE'].data, lines1)
     assert_array_equal(source2.lines['LINE'].data, lines2)
     cat2 = Catalog.from_sources([source1, source2])
     assert len(cat2) == 2
-    assert len(cat2.colnames) == 31
+    assert len(cat2.colnames) == 44
     assert list(cat['ID']) == list(cat2['ID'])
     assert_array_equal(source1.lines['LINE'].data, lines1)
     assert_array_equal(source2.lines['LINE'].data, lines2)
-    
-    
-    
-    
-
