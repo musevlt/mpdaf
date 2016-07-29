199a71dfe9097c1c6b40938f9a14da3530431a10
Simon Conseil <simon.conseil@univ-lyon1.fr>
2016-07-29 13:06:58 +0200

    Use pytest's parametrize

--- lib/mpdaf/sdetect/tests/test_catalog.py
+++ lib/mpdaf/sdetect/tests/test_catalog.py
@@ -2,27 +2,23 @@
 
 from __future__ import absolute_import, division, print_function
 
+import pytest
 from mpdaf.sdetect import Catalog
-
 from numpy.testing import assert_array_equal
 
 
-def test_catalog(source1, source2):
+@pytest.mark.parametrize('fmt,ncols', (('default', 44),
+                                       ('working', 42)))
+def test_catalog(source1, source2, fmt, ncols):
     source1.CUBE_V = '0.1'
     source2.CUBE_V = '0.2'
     lines1 = source1.lines['LINE'].data.copy()
     lines2 = source2.lines['LINE'].data.copy()
-    cat = Catalog.from_sources([source1, source2], fmt='working')
+    cat = Catalog.from_sources([source1, source2], fmt=fmt)
     print(cat)
     assert len(cat) == 2
-    assert len(cat.colnames) == 42
+    assert len(cat.colnames) == ncols
     assert list(cat['ID']) == [1, 32]
     assert list(cat['CUBE_V']) == ['0.1', '0.2']
-    assert_array_equal(source1.lines['LINE'].data, lines1)
-    assert_array_equal(source2.lines['LINE'].data, lines2)
-    cat2 = Catalog.from_sources([source1, source2])
-    assert len(cat2) == 2
-    assert len(cat2.colnames) == 44
-    assert list(cat['ID']) == list(cat2['ID'])
     assert_array_equal(source1.lines['LINE'].data, lines1)
     assert_array_equal(source2.lines['LINE'].data, lines2)
