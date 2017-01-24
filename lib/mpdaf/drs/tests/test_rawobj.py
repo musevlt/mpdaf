ade8286c768a81deb3b9e415a05d1581a17a5314
Simon Conseil <simon.conseil@univ-lyon1.fr>
2017-01-24 16:32:02 +0100

    Run tests for RawFile

--- lib/mpdaf/drs/tests/test_rawobj.py
+++ lib/mpdaf/drs/tests/test_rawobj.py
@@ -3,22 +3,29 @@
 from __future__ import absolute_import
 
 import numpy
-import os
 import pytest
+from os.path import join, exists
 from mpdaf.drs import RawFile
 
-from ...tests.utils import get_data_file
+from ...tests.utils import DATADIR
 
-DATA_PATH = get_data_file('drs', 'raw.fits')
-MISSING = not os.path.exists(DATA_PATH)
+EXTERN_DATADIR = join(DATADIR, 'extern')
+SERVER_DATADIR = '/home/gitlab-runner/mpdaf-test-data'
+
+if exists(EXTERN_DATADIR):
+    SUPP_FILES_PATH = EXTERN_DATADIR
+elif exists(SERVER_DATADIR):
+    SUPP_FILES_PATH = SERVER_DATADIR
+else:
+    SUPP_FILES_PATH = None
 
 
 @pytest.fixture
 def rawobj():
-    return RawFile(DATA_PATH)
+    return RawFile(join(SUPP_FILES_PATH, 'raw.fits'))
 
 
-@pytest.mark.skipif(MISSING, reason="Missing test data (data/drs/raw.fits)")
+@pytest.mark.skipif(not SUPP_FILES_PATH, reason="Missing test data (raw.fits)")
 def test_raw_init(rawobj):
     """Raw objects: tests initialization"""
     chan1 = rawobj.get_channel("CHAN01")
@@ -26,7 +33,7 @@ def test_raw_init(rawobj):
     assert shape == (rawobj.ny, rawobj.nx)
 
 
-@pytest.mark.skipif(MISSING, reason="Missing test data (data/drs/raw.fits)")
+@pytest.mark.skipif(not SUPP_FILES_PATH, reason="Missing test data (raw.fits)")
 def test_raw_mask(rawobj):
     """Raw objects: tests strimmed and overscan functionalities"""
     overscan = rawobj[1].data[24, 12]
