42235ffa6b72cd74f686a20958b9cc0b1bcafd6b
Simon Conseil <simon.conseil@univ-lyon1.fr>
2017-02-02 11:10:22 +0100

    Add test for broadcast_to_cube

--- lib/mpdaf/tools/tests/test_util.py
+++ lib/mpdaf/tools/tests/test_util.py
@@ -2,16 +2,18 @@
 
 from __future__ import absolute_import
 
+import numpy as np
 import os
+import pytest
 import warnings
 
-from mpdaf.tools import util
+from mpdaf.tools.util import chdir, deprecated, broadcast_to_cube
 
 
 def test_chdir(tmpdir):
     cwd = os.getcwd()
     tmp = str(tmpdir)
-    with util.chdir(tmp):
+    with chdir(tmp):
         assert tmp == os.getcwd()
 
     assert cwd == os.getcwd()
@@ -20,7 +22,7 @@ def test_chdir(tmpdir):
 def test_deprecated():
     msg = 'This function is deprecated'
 
-    @util.deprecated(msg)
+    @deprecated(msg)
     def func():
         pass
 
@@ -28,3 +30,17 @@ def test_deprecated():
         func()
         assert (w[0].message.args[0] ==
                 'Call to deprecated function `func`. ' + msg)
+
+
+def test_broadcast_to_cube():
+    shape = (5, 4, 3)
+
+    with pytest.raises(AssertionError):
+        broadcast_to_cube(np.zeros(5), (2, 2))
+
+    for s in (5, (4, 3), shape):
+        assert broadcast_to_cube(np.zeros(s), shape).shape == shape
+
+    for s in (4, (5, 3), (4, 4, 3)):
+        with pytest.raises(ValueError):
+            broadcast_to_cube(np.zeros(s), shape)
