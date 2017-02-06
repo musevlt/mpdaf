Fix PSF extraction with white-light weighting

lib/mpdaf/scripts/make_white_image.py
@@ -48,7 +48,7 @@ def make_white_image(inputfile, outputfile, verbose=False):
         cube.info()
 
     im = cube.mean(axis=0)
-    im.write(outputfile)
+    im.write(outputfile, savemask='nan')
     if verbose:
         print('Execution time {:.3f} seconds.'.format(time() - t0))
 

lib/mpdaf/sdetect/sea.py
@@ -358,20 +358,21 @@ def compute_optimal_spectrum(cube, mask, psf):
         psf = broadcast_to_cube(psf, cube.shape)
 
     # Normalize weights
-    psf = psf / np.sum(psf, axis=(1, 2))[:, np.newaxis, np.newaxis]
+    psf = psf * mask
+    psf /= np.sum(psf, axis=(1, 2))[:, np.newaxis, np.newaxis]
 
     data = cube.data.filled(np.nan)
 
     if cube._var is not None:
         var = cube.var.filled(np.nan)
-        d = np.nansum(mask * psf**2 / var, axis=(1, 2))
-        newdata = np.nansum(mask * psf * data / var, axis=(1, 2)) / d
-        newvar = np.nansum(mask * psf, axis=(1, 2)) / d
+        d = np.nansum(psf**2 / var, axis=(1, 2))
+        newdata = np.nansum(psf * data / var, axis=(1, 2)) / d
+        newvar = np.nansum(psf, axis=(1, 2)) / d
     else:
         warnings.warn('Extracting spectrum from a cube without variance',
                       MpdafWarning)
-        d = np.nansum(mask * psf**2, axis=(1, 2))
-        newdata = np.nansum(mask * psf * data, axis=(1, 2)) / d
+        d = np.nansum(psf**2, axis=(1, 2))
+        newdata = np.nansum(psf * data, axis=(1, 2)) / d
         newvar = None
 
     return Spectrum(wave=cube.wave, unit=cube.unit, data=newdata, var=newvar,

lib/mpdaf/sdetect/tests/test_source.py
@@ -469,9 +469,9 @@ def test_SEA2(minicube):
     # mask = broadcast_to(mask, cube.shape)
     # sp = np.nansum(cube.data * mask, axis=(1, 2))
     # npix_sky = np.sum((~cube.mask) & mask, axis=(1, 2))
-    assert_almost_equal(19.01, s.spectra['MUSE_WHITE'].data, decimal=2)
-    assert_almost_equal(8.64, s.spectra['MUSE_WHITE'].var, decimal=2)
-    assert_almost_equal(18.96, s.spectra['MUSE_WHITE_SKYSUB'].data, decimal=2)
+    assert_almost_equal(18.33, s.spectra['MUSE_WHITE'].data, decimal=2)
+    assert_almost_equal(8.33, s.spectra['MUSE_WHITE'].var, decimal=2)
+    assert_almost_equal(18.27, s.spectra['MUSE_WHITE_SKYSUB'].data, decimal=2)
 
 
 def test_add_FSF():