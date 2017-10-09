e2ead8bd115a11be1c1ffb544d7f21cd981c34a9
Simon Conseil <simon.conseil@univ-lyon1.fr>
2017-10-09 13:28:54 +0200

    Remove html_use_smartypants (deprecated)

--- doc/conf.py
+++ doc/conf.py
@@ -225,10 +225,6 @@ html_static_path = ['_static']
 # using the given strftime format.
 html_last_updated_fmt = '%d %b %Y'
 
-# If true, SmartyPants will be used to convert quotes and dashes to
-# typographically correct entities.
-html_use_smartypants = True
-
 # Custom sidebar templates, maps document names to template names.
 # html_sidebars = {}
 
