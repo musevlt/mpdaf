d8bf34e1447fef20769084c09909a6457b201bce
Simon Conseil <simon.conseil@univ-lyon1.fr>
2017-11-24 10:49:46 +0100

    version 2.4

--- lib/mpdaf/version.py
+++ lib/mpdaf/version.py
@@ -30,8 +30,8 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
-__version__ = '2.4dev'
-__date__ = '****/**/**'
+__version__ = '2.4'
+__date__ = '2017/11/24'
 
 try:
     from ._githash import __githash__, __dev_value__
