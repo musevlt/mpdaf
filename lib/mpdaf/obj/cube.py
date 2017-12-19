a79a242cdcf01318086052d9d40b65bb350ded4a
Roland Bacon <roland.bacon@univ-lyon1.fr>
2017-12-19 11:25:10 +0100

    add wavelength range in subcube_circle_aperture

--- lib/mpdaf/obj/cube.py
+++ lib/mpdaf/obj/cube.py
@@ -1985,7 +1985,7 @@ class Cube(ArithmeticMixin, DataArray):
 
     def subcube(self, center, size, lbda=None, unit_center=u.deg,
                 unit_size=u.arcsec, unit_wave=u.angstrom):
-        """Extracts a sub-cube around a position.
+        """Extracts a sub-cube around a position and for a wavelength range.
 
         Parameters
         ----------
@@ -2124,10 +2124,10 @@ class Cube(ArithmeticMixin, DataArray):
                     primary_header=fits.Header(self.primary_header),
                     filename=self.filename)
 
-    def subcube_circle_aperture(self, center, radius, unit_center=u.deg,
-                                unit_radius=u.arcsec):
+    def subcube_circle_aperture(self, center, radius, lbda=None, unit_center=u.deg,
+                unit_radius=u.arcsec, unit_wave=u.angstrom):
         """Extract a sub-cube that encloses a circular aperture of
-        a specified radius.
+        a specified radius and for a given wavelength range.
 
         Pixels outside the circle are masked.
 
@@ -2137,6 +2137,8 @@ class Cube(ArithmeticMixin, DataArray):
             The center of the aperture (y,x)
         radius : float
             The radius of the aperture.
+        lbda : (float, float) or None
+            If not None, tuple giving the wavelength range.
         unit_center : `astropy.units.Unit`
             The units of the center coordinates (degrees by default)
             The special value, None, indicates that the center is a
@@ -2145,6 +2147,9 @@ class Cube(ArithmeticMixin, DataArray):
             The units of the radius argument (arcseconds by default)
             The special value, None, indicates that the radius is
             specified in pixels.
+        unit_wave : `astropy.units.Unit`
+            Wavelengths unit (angstrom by default)
+            If None, inputs are in pixels
 
         Returns
         -------
