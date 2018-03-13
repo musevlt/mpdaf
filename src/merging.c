bb2ee4b7d9bc44a0d9f1ebc68bf2da35fef2b027
Simon Conseil <simon.conseil@univ-lyon1.fr>
2018-03-13 19:22:12 +0100

    Add our own version of strdup as it is not standard
    See https://stackoverflow.com/a/26284172 - Using strdup from strings.h
    with -std=c99 fails.

--- src/merging.c
+++ src/merging.c
@@ -24,6 +24,13 @@
  **************************************************************/
 
 
+char *strdup (const char *s) {
+    char *d = malloc (strlen (s) + 1);   // Space for length plus nul
+    if (d == NULL) return NULL;          // No memory
+    strcpy (d,s);                        // Copy the characters
+    return d;                            // Return the new string
+}
+
 // split input files list
 int split_files_list(char* input, char* filenames[]) {
     int nfiles=0;
