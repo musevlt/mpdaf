a6f9816702c6018b66b2dae31bf4a3c223c3e29f
Simon Conseil <simon.conseil@univ-lyon1.fr>
2018-04-23 05:12:34 -0300

    Remove the first (compound) model

--- lib/mpdaf/MUSE/nfm.py
+++ lib/mpdaf/MUSE/nfm.py
@@ -6,7 +6,7 @@ import shutil
 import tqdm
 
 from astropy.modeling import fitting, custom_model
-from astropy.modeling.models import Moffat2D, Const2D, Linear1D
+from astropy.modeling.models import Moffat2D, Linear1D
 from astropy.stats import sigma_clip
 from astropy.table import Table
 from astropy.table import vstack
@@ -15,8 +15,7 @@ from photutils.detection import find_peaks
 from scipy.special import j1, jn_zeros
 
 from ..obj import Image, Cube
-from ..obj.fitting import (SmoothDisk2D, SmoothOuterDisk2D, EllipticalMoffat2D,
-                           AiryDisk2D)
+from ..obj.fitting import AiryDisk2D
 
 # Magic parameters from the IDL code
 ACT = .19    # [m] Inter-actuator distance in the pupil plane
@@ -63,72 +62,8 @@ def correction_radius(lbda):
     # return a*lbda + b
 
 
-def mphd_model(shape, lbda, with_ellipticity=False, R_0=None, verbose=False,
-               with_bounds=False, peak=1):
-    # center
-    x_0, y_0 = np.array(shape)/2
-
-    R_c = correction_radius(lbda) if R_0 is None else R_0
-    if verbose:
-        print(f'Initial Rc for $\lambda$ {lbda} is {R_c:.2f}')
-
-    if with_ellipticity:
-        Mpeak = EllipticalMoffat2D(x_0=x_0, y_0=y_0, alpha=1., name='Mpeak',
-                                   amplitude=peak/2)
-        Mhalo = EllipticalMoffat2D(x_0=x_0, y_0=y_0, alpha=2., name='Mhalo')
-        # Mpeak.amplitude.min = 0
-    else:
-        Mpeak = Moffat2D(x_0=x_0, y_0=y_0, alpha=1., name='Mpeak',
-                         amplitude=peak)
-        Mhalo = Moffat2D(x_0=x_0, y_0=y_0, alpha=2., name='Mhalo',
-                         amplitude=peak/2)
-
-    W = SmoothDisk2D(x_0=x_0, y_0=y_0, R_0=R_c, name='W')
-    Winv = SmoothOuterDisk2D(x_0=x_0, y_0=y_0, R_0=R_c, name='Winv')
-    W.amplitude.fixed = True
-    Winv.amplitude.fixed = True
-
-    if R_0 is not None:
-        W.R_0.fixed = True
-        Winv.R_0.fixed = True
-    else:
-        Winv.R_0.tied = lambda g: g['W'].R_0
-
-    if with_bounds:
-        Mpeak.alpha.min = 0
-        Mhalo.alpha.min = 0
-        W.R_0.min = R_c - 4
-        W.R_0.max = R_c + 4
-
-    R_airy = compute_airy_radius(lbda)
-    # airy = Gaussian2D(x_mean=x_0, y_mean=y_0, x_stddev=R_airy,
-    #                   y_stddev=R_airy, name='Airy')
-    # airy.theta.fixed = True
-    airy = AiryDisk2D(x_0=x_0, y_0=y_0, radius=R_airy*2, name='Airy',
-                      amplitude=peak)
-    # airy.radius.fixed = True
-    # airy.amplitude.min = 1e3
-    if verbose:
-        print(f'Initial $R_airy$ for $\lambda$ {lbda} is {R_airy:.2f}')
-
-    Bp = Const2D(name='Bp', amplitude=0)
-
-    model = (((Mpeak + Bp) * W + airy)) + (Mhalo * Winv)
-
-    # centers must be tied together
-    for mod in model[1:]:
-        # if mod.name == 'Airy':
-        #     mod.x_mean.tied = lambda g: g['Mpeak'].x_0
-        #     mod.y_mean.tied = lambda g: g['Mpeak'].y_0
-        if mod.name != 'Bp':
-            mod.x_0.tied = lambda g: g['Mpeak'].x_0
-            mod.y_0.tied = lambda g: g['Mpeak'].y_0
-
-    return model
-
-
 @custom_model
-def mphd(x, y, x_0=0, y_0=0, R_c=1, Airy_radius=1, Airy_amplitude=1,
+def MPHD(x, y, x_0=0, y_0=0, R_c=1, Airy_radius=1, Airy_amplitude=1,
          Mpeak_amplitude=1, Mpeak_alpha=1, Mpeak_gamma=1, Bp=0,
          Mhalo_amplitude=1, Mhalo_gamma=1):
 
@@ -158,8 +93,8 @@ def mphd(x, y, x_0=0, y_0=0, R_c=1, Airy_radius=1, Airy_amplitude=1,
     return Mpeak * W + Airy + Mhalo * (1 - W)
 
 
-def mphd_model2(shape, lbda, R_0=None, verbose=False, with_bounds=False,
-                peak=1):
+def init_mphd_model(shape, lbda, R_0=None, verbose=False, with_bounds=False,
+                    peak=1):
     # initial parameters
     x_0, y_0 = np.array(shape)/2
     R_c = (1.22 * correction_radius(lbda)) if R_0 is None else R_0
@@ -177,7 +112,7 @@ def mphd_model2(shape, lbda, R_0=None, verbose=False, with_bounds=False,
         print(f'Initial R_c for lambda {lbda} is {R_c:.2f}')
         print(f'Initial R_airy for lambda {lbda} is {R_airy:.2f}')
 
-    model = mphd(x_0=x_0, y_0=y_0, R_c=R_c, Airy_radius=R_airy*2,
+    model = MPHD(x_0=x_0, y_0=y_0, R_c=R_c, Airy_radius=R_airy*2,
                  Airy_amplitude=peak,
                  Mpeak_amplitude=peak, Mpeak_alpha=1, Mpeak_gamma=1, Bp=0,
                  Mhalo_amplitude=Mhalo_amplitude, Mhalo_gamma=ah)
@@ -224,16 +159,12 @@ def moffat_to_fwhm(alpha, gamma):
 
 
 def fit_mphd(img, lbda, use_var=False, maxiter=2000, with_ellipticity=False,
-             R_c=None, verbose=True, savefig=None, return_all=False,
-             custom_model=True):
+             R_c=None, verbose=True, savefig=None, return_all=False):
     yy, xx = np.mgrid[:img.shape[0], :img.shape[1]]
     fitter = fitting.LevMarLSQFitter()
-    if custom_model:
-        model = mphd_model2(img.shape, lbda, R_0=R_c, verbose=verbose,
+    model = init_mphd_model(img.shape, lbda, R_0=R_c, verbose=verbose,
                             peak=img.data.max())
-    else:
-        model = mphd_model(img.shape, lbda, with_ellipticity=with_ellipticity,
-                           R_0=R_c, verbose=verbose, peak=img.data.max())
+
     kwargs = dict(maxiter=maxiter)
     if use_var:
         kwargs['weights'] = 1 / np.sqrt(img.var.filled(np.inf))
@@ -334,11 +265,7 @@ def plot_residual(im, fit, figsize=(12, 8), title=None):
     for ax, img in zip([ax1, ax2, ax3], [im, ima_fit, im - ima_fit]):
         img.plot(ax=ax, zscale=True, colorbar='v')
 
-    compound = fit.n_submodels() > 1
-    if compound:
-        fy = int(fit['Mpeak'].x_0.value + 0.5)
-    else:
-        fy = int(fit.x_0.value + 0.5)
+    fy = int(fit.x_0.value + 0.5)
 
     ax = fig.add_subplot(gs[1, :])
 
@@ -346,30 +273,22 @@ def plot_residual(im, fit, figsize=(12, 8), title=None):
     ax.plot(ima_fit.data[fy], label='sum', lw=2)
 
     kwargs = dict(alpha=0.6, linestyle='--')
-    if compound:
-        peak_fit = fit['Mpeak'] * fit['W']
-        airy_fit = fit['Airy'] * fit['W']
-        halo_fit = fit['Mhalo'] * fit['Winv']
-        ax.plot(peak_fit(xx, yy)[fy], label='core', **kwargs)
-        ax.plot(airy_fit(xx, yy)[fy], label='airy', **kwargs)
-        ax.plot(halo_fit(xx, yy)[fy], label='halo', **kwargs)
-    else:
-        x_0, y_0 = fit.x_0.value, fit.y_0.value
-        rr = ((xx - fit.x_0.value) ** 2 + (yy - y_0) ** 2)
-        rr2 = np.sqrt(rr)
-        W = 1 / (1 + np.exp((rr2 - fit.R_c.value)))
-        m = Moffat2D(x_0=x_0, y_0=y_0, amplitude=fit.Mpeak_amplitude.value,
-                     alpha=fit.Mpeak_alpha.value, gamma=fit.Mpeak_gamma.value)
-        ax.plot((W * (m(xx, yy) + fit.Bp.value))[fy], label='core', **kwargs)
+    x_0, y_0 = fit.x_0.value, fit.y_0.value
+    rr = ((xx - fit.x_0.value) ** 2 + (yy - y_0) ** 2)
+    rr2 = np.sqrt(rr)
+    W = 1 / (1 + np.exp((rr2 - fit.R_c.value)))
+    m = Moffat2D(x_0=x_0, y_0=y_0, amplitude=fit.Mpeak_amplitude.value,
+                 alpha=fit.Mpeak_alpha.value, gamma=fit.Mpeak_gamma.value)
+    ax.plot((W * (m(xx, yy) + fit.Bp.value))[fy], label='core', **kwargs)
 
-        m = AiryDisk2D(amplitude=fit.Airy_amplitude.value, x_0=x_0,
-                       y_0=y_0, radius=fit.Airy_radius.value)
-        ax.plot(m(xx, yy)[fy], label='airy', **kwargs)
+    m = AiryDisk2D(amplitude=fit.Airy_amplitude.value, x_0=x_0,
+                   y_0=y_0, radius=fit.Airy_radius.value)
+    ax.plot(m(xx, yy)[fy], label='airy', **kwargs)
 
-        m = Moffat2D(x_0=x_0, y_0=y_0,
-                     amplitude=fit.Mhalo_amplitude.value,
-                     alpha=BETA_H, gamma=fit.Mhalo_gamma.value)
-        ax.plot(((1 - W) * m(xx, yy))[fy], label='halo', **kwargs)
+    m = Moffat2D(x_0=x_0, y_0=y_0,
+                 amplitude=fit.Mhalo_amplitude.value,
+                 alpha=BETA_H, gamma=fit.Mhalo_gamma.value)
+    ax.plot(((1 - W) * m(xx, yy))[fy], label='halo', **kwargs)
 
     ax.set_yscale('log')
     ax.set_ylim((1, ax.get_ylim()[1]))
@@ -462,16 +381,6 @@ def plot_all_results(filename, plot_bad_fits=False):
     ax.legend()
     ax.set_ylim((0, 3))
     ax.set_title('$beta_{peak}$')
-
-    # beta halo
-    # ax = next(axit)
-    # for i in starids:
-    #     res = t[t['starid'] == i]
-    #     ax.plot(res['lbda'], BETA_H, '-.^', alpha=0.6,
-    #             label=str(i))
-    # ax.legend()
-    # ax.set_ylim((0, 3))
-    # ax.set_title('$beta_{halo}$')
 
     # airy
     ax = next(axit)
