b2a27e75f98431c6462d8ff29affe3feafa5e987
Simon Conseil <simon.conseil@univ-lyon1.fr>
2018-08-09 18:24:28 +0200

    setup - add 3.7 classifier, include license in wheel

--- setup.py
+++ setup.py
@@ -259,6 +259,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Scientific/Engineering :: Astronomy',
         'Topic :: Scientific/Engineering :: Physics'
