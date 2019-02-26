d146fa151b71fb39fe7bdd41446c7a84e8af0e44
Simon Conseil <simon.conseil@univ-lyon1.fr>
2019-02-26 12:41:56 +0100

    Update test

--- lib/mpdaf/MUSE/tests/test_fsf.py
+++ lib/mpdaf/MUSE/tests/test_fsf.py
@@ -7,18 +7,22 @@ from numpy.testing import assert_allclose
 
 
 def test_get_FSF_from_cube_keywords():
-    cube = Cube(get_data_file('sdetect', 'minicube.fits'))
-    with pytest.raises(IOError):
-        # This cube has no FSF info
-        PSF, fwhm_pix, fwhm_arcsec = get_FSF_from_cube_keywords(cube, 13)
+    # This cube has no FSF info
+    with pytest.raises(ValueError):
+        FSFModel.read(get_data_file('sdetect', 'minicube.fits'))
 
-    cube = Cube(get_data_file('sdetect', 'subcub_mosaic.fits'))
+    cubename = get_data_file('sdetect', 'subcub_mosaic.fits')
+    cube = Cube(cubename)
+
+    # Read FSF model with the old method
     PSF, fwhm_pix, fwhm_arcsec = get_FSF_from_cube_keywords(cube, 13)
 
-    assert len(PSF) == 9
-    assert len(fwhm_pix) == 9
-    assert_allclose(fwhm_pix[0] * 0.2, fwhm_arcsec[0])
+    # Read FSF model from file
+    model = FSFModel.read(cubename)
+    assert model.model == 'MOFFAT1'
+    assert_allclose(model.get_fwhm(cube.wave.coord()), fwhm_arcsec[0])
 
-    model = FSFModel(get_data_file('sdetect', 'subcub_mosaic.fits'))
+    # Read FSF model from cube
+    model = FSFModel.read(cube)
     assert model.model == 'MOFFAT1'
     assert_allclose(model.get_fwhm(cube.wave.coord()), fwhm_arcsec[0])
