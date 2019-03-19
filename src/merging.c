faac8bd7406651f57983380452e17b88af224d3b
Simon Conseil <simon.conseil@univ-lyon1.fr>
2019-03-19 12:44:56 +0100

    cfitsio combine - read by plane instead of by row

--- src/merging.c
+++ src/merging.c
@@ -63,15 +63,15 @@ int get_max_threads(int nfiles, int typ_var) {
         exit(EXIT_FAILURE);
     }
 
-    int num_nthreads = limit.rlim_cur/nfiles * 0.9;
+    int num_nthreads = limit.rlim_cur / nfiles * 0.9;
     if (1000/nfiles < num_nthreads) {
         //limit of cfitsio
-        num_nthreads = 1000/nfiles;
+        num_nthreads = 1000 / nfiles;
     }
     printf("num_nthreads: %d\n", num_nthreads);
 
     if (typ_var==0) {
-        num_nthreads = num_nthreads/2;
+        num_nthreads = num_nthreads / 2;
     }
 
     int nthreads=1;
@@ -191,8 +191,7 @@ int mpdaf_merging_median(char* input, double* data, int* expmap, int* valid_pix)
         int *indx;
         double *pix[MAX_FILES_PER_THREAD], *wdata;
         long npixels = naxes[0];
-        for (i=0; i<nfiles; i++)
-        {
+        for (i=0; i<nfiles; i++) {
             pix[i] = (double *) malloc(npixels * sizeof(double));
             if (pix[i] == NULL) {
                 printf("Memory allocation error\n");
@@ -203,25 +202,19 @@ int mpdaf_merging_median(char* input, double* data, int* expmap, int* valid_pix)
         wdata = (double *) malloc(nfiles * sizeof(double));
         indx = (int *) malloc(nfiles * sizeof(int));
 
-        for (firstpix[2] = limits[0]; firstpix[2] <= limits[1]; firstpix[2]++)
-        {
-            for (firstpix[1] = 1; firstpix[1] <= naxes[1]; firstpix[1]++)
-            {
+        for (firstpix[2] = limits[0]; firstpix[2] <= limits[1]; firstpix[2]++) {
+            for (firstpix[1] = 1; firstpix[1] <= naxes[1]; firstpix[1]++) {
                 int index0 = (firstpix[1]-1)*naxes[0] + (firstpix[2]-1)*naxes[0]*naxes[1];
 
-                for (i=0; i<nfiles; i++)
-                {
+                for (i=0; i<nfiles; i++) {
                     if (fits_read_pix(fdata[i], TDOUBLE, firstpix, npixels, NULL, pix[i],
                                 NULL, &status))
                         break;
                 }
-                for(ii=0; ii< npixels; ii++)
-                {
+                for(ii=0; ii< npixels; ii++) {
                     n = 0;
-                    for (i=0; i<nfiles; i++)
-                    {
-                        if (!isnan(pix[i][ii]))
-                        {
+                    for (i=0; i<nfiles; i++) {
+                        if (!isnan(pix[i][ii])) {
                             wdata[n] = pix[i][ii];
                             indx[n] = n;
                             n = n + 1;
@@ -229,18 +222,13 @@ int mpdaf_merging_median(char* input, double* data, int* expmap, int* valid_pix)
                         }
                     }
                     int index = ii + index0;
-                    if (n==0)
-                    {
+                    if (n==0) {
                         data[index] = NAN; //mean value
                         expmap[index] = 0; //exp map
-                    }
-                    else if (n==1)
-                    {
+                    } else if (n==1) {
                         data[index] = wdata[0]; //mean value
                         expmap[index] = 1; //exp map
-                    }
-                    else
-                    {
+                    } else {
                         data[index] = mpdaf_median(wdata,n,indx);
                         expmap[index] = n;
                     }
@@ -251,29 +239,26 @@ int mpdaf_merging_median(char* input, double* data, int* expmap, int* valid_pix)
                 time(&now);
                 info = localtime(&now);
                 strftime(buffer,80,"%x - %I:%M%p", info);
-                if(strcmp(buffer,begin) != 0)
-                {
+                if(strcmp(buffer,begin) != 0) {
                     printf("%s %3.1f%%\n", buffer, (firstpix[2]-limits[0])*100.0/(limits[1]-limits[0]));
                     fflush(stdout);
                     strcpy(begin, buffer);
                 }
             }
         }
-        for (i=0; i<nfiles; i++)
-        {
+        for (i=0; i<nfiles; i++) {
             #pragma omp atomic
             valid_pix[i] += valid[i];
         }
+
         free(wdata);
         free(indx);
-        for (i=0; i<nfiles; i++)
-        {
+        for (i=0; i<nfiles; i++) {
             free(pix[i]);
             fits_close_file(fdata[i], &status);
         }
 
-        if (status)
-        {
+        if (status) {
             fits_report_error(stderr, status);
             exit(EXIT_FAILURE);
         }
@@ -289,7 +274,22 @@ int mpdaf_merging_median(char* input, double* data, int* expmap, int* valid_pix)
 // var=0: 'propagate'
 // var=1:  'stat_mean'
 // var=2:  'stat_one'
-int mpdaf_merging_sigma_clipping(char* input, double* data, double* var, int* expmap, double* scale, double*offset, int* selected_pix, int* valid_pix, int nmax, double nclip_low, double nclip_up, int nstop, int typ_var, int mad)
+int mpdaf_merging_sigma_clipping(
+    char* input,
+    double* data,
+    double* var,
+    int* expmap,
+    double* scale,
+    double*offset,
+    int* selected_pix,
+    int* valid_pix,
+    int nmax,
+    double nclip_low,
+    double nclip_up,
+    int nstop,
+    int typ_var,
+    int mad
+    )
 {
     char* filenames[MAX_FILES];
     char buffer[80], begin[80];
@@ -363,7 +363,7 @@ int mpdaf_merging_sigma_clipping(char* input, double* data, double* var, int* ex
         double *pix[MAX_FILES_PER_THREAD], *pixvar[MAX_FILES_PER_THREAD], *wdata, *wvar=NULL;
         int *indx, *files_id;
         double x[3];
-        long npixels = naxes[0];
+        long npixels = naxes[0] * naxes[1];
         for (i=0; i<nfiles; i++)
         {
             pix[i] = (double *) malloc(npixels * sizeof(double));
@@ -392,95 +392,74 @@ int mpdaf_merging_sigma_clipping(char* input, double* data, double* var, int* ex
 
         for (firstpix[2] = limits[0]; firstpix[2] <= limits[1]; firstpix[2]++)
         {
-            for (firstpix[1] = 1; firstpix[1] <= naxes[1]; firstpix[1]++)
-            {
-                int index0 = (firstpix[1]-1)*naxes[0] + (firstpix[2]-1)*naxes[0]*naxes[1];
+            int index0 = (firstpix[2] - 1) * npixels;
 
-                for (i=0; i<nfiles; i++)
-                {
-                    if (fits_read_pix(fdata[i], TDOUBLE, firstpix, npixels, NULL, pix[i],
-                                NULL, &status))
+            // read data values for the current plane, and optionally stat
+            for (i=0; i<nfiles; i++) {
+                if (fits_read_pix(fdata[i], TDOUBLE, firstpix, npixels,
+                                  NULL, pix[i], NULL, &status))
+                    break;
+            }
+            if (typ_var==0) {
+                for (i=0; i<nfiles; i++) {
+                    if (fits_read_pix(fvar[i], TDOUBLE, firstpix, npixels,
+                                      NULL, pixvar[i], NULL, &status))
                         break;
                 }
-                if (typ_var==0)
-                {
-                    for (i=0; i<nfiles; i++)
-                    {
-                        if (fits_read_pix(fvar[i], TDOUBLE, firstpix, npixels, NULL, pixvar[i],
-                                    NULL, &status))
-                            break;
+            }
+
+            for(ii=0; ii< npixels; ii++) {
+                n = 0;
+                for (i=0; i<nfiles; i++) {
+                    if (!isnan(pix[i][ii])) {
+                        wdata[n] = (offset[i] + pix[i][ii]) * scale[i];
+                        files_id[n] = i;
+                        indx[n] = n;
+                        if (typ_var==0) {
+                            wvar[n] = pixvar[i][ii]*scale[i]*scale[i];
+                        }
+                        n += 1;
+                        valid[i] += 1;
                     }
                 }
-                for(ii=0; ii< npixels; ii++)
-                {
-                    n = 0;
-                    for (i=0; i<nfiles; i++)
-                    {
-                        if (!isnan(pix[i][ii]))
-                        {
-                            wdata[n] = (offset[i] + pix[i][ii]) * scale[i];
-                            files_id[n] = i;
-                            indx[n] = n;
-                            if (typ_var==0)
-                            {
-                                wvar[n] = pixvar[i][ii]*scale[i]*scale[i];
-                            }
-                            n = n + 1;
-                            valid[i] = valid[i] + 1;
-                        }
-                    }
-                    int index = ii + index0;
-                    if (n==0)
-                    {
-                        data[index] = NAN; //mean value
-                        expmap[index] = 0; //exp map
-                        var[index] = NAN;  //var
-                    }
-                    else if (n==1)
-                    {
-                        data[index] = wdata[0]; //mean value
-                        expmap[index] = 1;      //exp map
-                        if (typ_var==0)         //var
-                            var[index] = wvar[0];
-                        else
-                            var[index] = NAN;
-                        select[files_id[0]] += 1;
-                    }
+                int index = ii + index0;
+                if (n==0) {
+                    data[index] = NAN; //mean value
+                    expmap[index] = 0; //exp map
+                    var[index] = NAN;  //var
+                } else if (n==1) {
+                    data[index] = wdata[0]; //mean value
+                    expmap[index] = 1;      //exp map
+                    if (typ_var==0)         //var
+                        var[index] = wvar[0];
                     else
-                    {
-                        if (mad==1)
-                        {
-                            mpdaf_mean_madsigma_clip(wdata, n, x, nmax, nclip_low, nclip_up, nstop, indx);
-                        }
-                        else
-                        {
-                            mpdaf_mean_sigma_clip(wdata, n, x, nmax, nclip_low, nclip_up, nstop, indx);
-                        }
-                        data[index] = x[0];//mean value
-                        expmap[index] = x[2];//exp map
-                        if (typ_var==0)
-                        {
-                            var[index] = mpdaf_sum(wvar,x[2],indx)/x[2]/x[2];
-                        }
-                        else
-                        {
-                            if (x[2]>1)
-                            {
-                                var[index] = (x[1]*x[1]);//var
-                                if (typ_var==1)
-                                {
-                                    var[index] /= (x[2]-1);
-                                }
+                        var[index] = NAN;
+                    select[files_id[0]] += 1;
+                } else {
+                    if (mad==1) {
+                        mpdaf_mean_madsigma_clip(wdata, n, x, nmax, nclip_low,
+                                                 nclip_up, nstop, indx);
+                    } else {
+                        mpdaf_mean_sigma_clip(wdata, n, x, nmax, nclip_low,
+                                              nclip_up, nstop, indx);
+                    }
+
+                    data[index] = x[0];   // mean value
+                    expmap[index] = x[2]; // exp map
+                    if (typ_var==0) {     // var
+                        var[index] = mpdaf_sum(wvar, x[2], indx) / (x[2] * x[2]);
+                    } else {
+                        if (x[2]>1) {
+                            var[index] = (x[1] * x[1]);
+                            if (typ_var==1) {
+                                var[index] /= (x[2] - 1);
                             }
-                            else
-                            {
-                                var[index] = NAN;//var
-                            }
+                        } else {
+                            var[index] = NAN;
                         }
-                        for (i=0; i<x[2]; i++)
-                        {
-                            select[files_id[indx[i]]] += 1;
-                        }
+                    }
+                    for (i=0; i<x[2]; i++) {
+                        select[files_id[indx[i]]] += 1;
                     }
                 }
             }
@@ -489,41 +468,38 @@ int mpdaf_merging_sigma_clipping(char* input, double* data, double* var, int* ex
                 time(&now);
                 info = localtime(&now);
                 strftime(buffer,80,"%x - %I:%M%p", info);
-                if(strcmp(buffer,begin) != 0)
-                {
-                    printf("%s %3.1f%%\n", buffer, firstpix[2]*100.0/(limits[1]-limits[0]));
+                if(strcmp(buffer,begin) != 0) {
+                    printf("%s %3.1f%%\n", buffer,
+                           firstpix[2] * 100.0 / (limits[1] - limits[0]));
                     fflush(stdout);
                     strcpy(begin, buffer);
                 }
             }
         }
-        for (i=0; i<nfiles; i++)
-        {
+
+        for (i=0; i<nfiles; i++) {
             #pragma omp atomic
             valid_pix[i] += valid[i];
             #pragma omp atomic
             selected_pix[i] += select[i];
         }
+
         free(wdata);
         free(indx);
         free(files_id);
-        for (i=0; i<nfiles; i++)
-        {
+        for (i=0; i<nfiles; i++) {
             free(pix[i]);
             fits_close_file(fdata[i], &status);
         }
-        if (typ_var==0)
-        {
+        if (typ_var==0) {
             free(wvar);
-            for (i=0; i<nfiles; i++)
-            {
+            for (i=0; i<nfiles; i++) {
                 free(pixvar[i]);
                 fits_close_file(fvar[i], &status);
             }
         }
 
-        if (status)
-        {
+        if (status) {
             fits_report_error(stderr, status);
             exit(EXIT_FAILURE);
         }
